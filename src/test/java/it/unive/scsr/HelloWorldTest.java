8d04c9e7fcc36c16fe6df65ed48ec6af22d34a2b
Giacomo Zanatta <giacomo@Giacomos-MacBook-Pro.local>
2026-03-17 18:34:37 +0100

    fix test case

--- src/test/java/it/unive/scsr/HelloWorldTest.java
+++ src/test/java/it/unive/scsr/HelloWorldTest.java
@@ -3,6 +3,7 @@ package it.unive.scsr;
 import it.unive.lisa.AnalysisException;
 import it.unive.lisa.DefaultConfiguration;
 import it.unive.lisa.LiSA;
+import it.unive.lisa.analysis.ConstantValuePropagation;
 import it.unive.lisa.analysis.heap.MonolithicHeap;
 import it.unive.lisa.conf.LiSAConfiguration;
 import it.unive.lisa.imp.IMPFrontend;
@@ -12,7 +13,10 @@ import it.unive.lisa.outputs.HtmlResults;
 import it.unive.lisa.program.Program;
 import org.junit.Test;
 
+import static it.unive.lisa.DefaultConfiguration.*;
+
 public class HelloWorldTest {
+
     @Test
     public void testHelloWorld() throws ParsingException, AnalysisException {
         // we parse the program to get the CFG representation of the code in it
@@ -26,9 +30,10 @@ public class HelloWorldTest {
 
         // we specify the visual format of the analysis results
         conf.outputs.add(new HtmlInputs(true));
-
+        conf.outputs.add(new HtmlResults<>(true));
         // we specify the analysis that we want to execute
-
+        conf.analysis = simpleDomain(defaultHeapDomain(), new ConstantValuePropagation(), defaultTypeDomain());
+        //conf.analysis = DefaultConfiguration.defaultAbstractDomain();
         // we instantiate LiSA with our configuration
         LiSA lisa = new LiSA(conf);
 

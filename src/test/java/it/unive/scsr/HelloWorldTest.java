9f6963bd1d0a4e0a89da7d5a75df19d1a08ce898
890939NicolaMaso <890939@stud.unive.it>
2026-03-21 18:11:56 +0100

    Lab Lesson 1 — Tested LiSA locally

--- src/test/java/it/unive/scsr/HelloWorldTest.java
+++ src/test/java/it/unive/scsr/HelloWorldTest.java
@@ -3,14 +3,18 @@ package it.unive.scsr;
 import it.unive.lisa.AnalysisException;
 import it.unive.lisa.DefaultConfiguration;
 import it.unive.lisa.LiSA;
+import it.unive.lisa.analysis.ConstantValuePropagation;
 import it.unive.lisa.analysis.heap.MonolithicHeap;
 import it.unive.lisa.conf.LiSAConfiguration;
 import it.unive.lisa.imp.IMPFrontend;
 import it.unive.lisa.imp.ParsingException;
+import it.unive.lisa.lattices.ConstantValue;
 import it.unive.lisa.outputs.HtmlInputs;
 import it.unive.lisa.outputs.HtmlResults;
 import it.unive.lisa.program.Program;
 import org.junit.Test;
+
+import static it.unive.lisa.DefaultConfiguration.*;
 
 public class HelloWorldTest {
     @Test
@@ -26,8 +30,11 @@ public class HelloWorldTest {
 
         // we specify the visual format of the analysis results
         conf.outputs.add(new HtmlInputs(true));
+        conf.outputs.add(new HtmlResults<>(true));
 
         // we specify the analysis that we want to execute
+        conf.analysis = simpleDomain(defaultHeapDomain(),new ConstantValuePropagation(),defaultTypeDomain());
+        //conf.analysis = DefaultConfiguration.defaultAbstractDomain();
 
         // we instantiate LiSA with our configuration
         LiSA lisa = new LiSA(conf);

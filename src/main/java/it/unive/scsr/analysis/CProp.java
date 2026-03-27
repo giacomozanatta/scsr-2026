22b2c4f8a033fe0d8a5995a0606c9c4e26ca9820
LucaBarban <45258881+LucaBarban@users.noreply.github.com>
2026-03-27 13:34:24 +0100

    Adding constant propagation

--- src/main/java/it/unive/scsr/analysis/CProp.java
+++ src/main/java/it/unive/scsr/analysis/CProp.java
@@ -1,7 +1,100 @@
 package it.unive.scsr.analysis;
 
-public class CProp /* extends ... */{
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.dataflow.DataflowDomain;
+import it.unive.lisa.analysis.dataflow.DefiniteSet;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.value.*;
+import it.unive.lisa.symbolic.value.operator.AdditionOperator;
+import it.unive.lisa.symbolic.value.operator.DivisionOperator;
+import it.unive.lisa.symbolic.value.operator.MultiplicationOperator;
+import it.unive.lisa.symbolic.value.operator.SubtractionOperator;
+import it.unive.lisa.symbolic.value.operator.unary.NumericNegation;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class CProp extends DataflowDomain<DefiniteSet<CPropSetElem>, CPropSetElem> /* extends ... */ {
+    public static Integer parser(ValueExpression expression, DefiniteSet<CPropSetElem> elems) {
+        if (expression == null)
+            return null;
+
+        if (expression instanceof Constant ex) {
+            Object tmp = ex.getValue();
+            return tmp instanceof Integer ? (Integer) tmp : null;
+        }
+
+        if (expression instanceof Identifier ex) {
+            for (CPropSetElem elem : elems.getDataflowElements())
+                if (elem.id().equals(ex))
+                    return elem.constant();
+            return null;
+        }
+
+        if (expression instanceof UnaryExpression un) {
+            Operator op = un.getOperator();
+            ValueExpression ex = (ValueExpression) un.getExpression();
+
+            Integer tmpval = parser(ex, elems);
+
+            return tmpval == null ? null : tmpval * (op instanceof NumericNegation ? -1 : 1);
+        }
+
+        if (expression instanceof BinaryExpression ex) {
+            Integer left = parser((ValueExpression) ex.getLeft(), elems);
+            Integer right = parser((ValueExpression) ex.getRight(), elems);
+            Operator op = ex.getOperator();
+
+            if (left == null || right == null)
+                return null;
+            else if (op instanceof AdditionOperator) {
+                return left + right;
+            } else if (op instanceof SubtractionOperator) {
+                return left - right;
+            } else if (op instanceof MultiplicationOperator) {
+                return left * right;
+            } else if (op instanceof DivisionOperator) {
+                return left / right;
+            }
+
+            return null;
+        }
+
+        return null;
+    }
+
+    @Override
+    public Set<CPropSetElem> gen(DefiniteSet<CPropSetElem> cPropSetElems, Identifier identifier, ValueExpression valueExpression, ProgramPoint programPoint) throws SemanticException {
+        Integer tmp = parser(valueExpression, cPropSetElems);
+        return tmp != null ? Collections.singleton(new CPropSetElem(identifier, tmp)) : Collections.emptySet();
+    }
+
+    @Override
+    public Set<CPropSetElem> gen(DefiniteSet<CPropSetElem> cPropSetElems, ValueExpression valueExpression, ProgramPoint programPoint) throws SemanticException {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public Set<CPropSetElem> kill(DefiniteSet<CPropSetElem> cPropSetElems, Identifier identifier, ValueExpression valueExpression, ProgramPoint programPoint) throws SemanticException {
+        Set<CPropSetElem> killSet = new HashSet<>();
+
+        for (CPropSetElem elem : cPropSetElems)
+            if (elem.id().equals(identifier))
+                killSet.add(elem);
+
+        return killSet;
+    }
+
+    @Override
+    public Set<CPropSetElem> kill(DefiniteSet<CPropSetElem> cPropSetElems, ValueExpression valueExpression, ProgramPoint programPoint) throws SemanticException {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public DefiniteSet<CPropSetElem> makeLattice() {
+        return new DefiniteSet<>();
+    }
     // IMPLEMENTATION NOTE:
 
     // - Implement your solution using the DefiniteDataFlowDomain.
--- src/main/java/it/unive/scsr/analysis/CPropSetElem.java
+++ src/main/java/it/unive/scsr/analysis/CPropSetElem.java
@@ -1,15 +1,27 @@
 package it.unive.scsr.analysis;
 
-public class CPropSetElem /* implements ... */{
-    /**
-     * IMPLEMENT THIS CLASS
-     * the code below is outside of the scope of the course.
-     * You can uncomment it to get your code to compile.
-     * Be aware that the code is written expecting that a field named "id"
-     * and a field named "constant" exist in this class.
-     */
+import it.unive.lisa.analysis.ScopeToken;
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.dataflow.DataflowElement;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.value.Identifier;
+import it.unive.lisa.util.representation.ListRepresentation;
+import it.unive.lisa.util.representation.StringRepresentation;
+import it.unive.lisa.util.representation.StructuredRepresentation;
 
-    /*
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * @param id IMPLEMENT THIS CLASS
+ *           the code below is outside of the scope of the course.
+ *           You can uncomment it to get your code to compile.
+ *           Be aware that the code is written expecting that a field named "id"
+ *           and a field named "constant" exist in this class.
+ */
+public record CPropSetElem(Identifier id,
+                           Integer constant) implements DataflowElement<CPropSetElem> /* implements ... */ {
+
     @Override
     public StructuredRepresentation representation() {
         return new ListRepresentation(
@@ -18,20 +30,22 @@ public class CPropSetElem /* implements ... */{
     }
 
     @Override
-    public CPropSetElem pushScope(ScopeToken scope)
-            throws SemanticException {
-        return this;
-    }
-
-    @Override
-    public CPropSetElem popScope(ScopeToken scope)
-            throws SemanticException {
-        return this;
+    public Collection<Identifier> getInvolvedIdentifiers() {
+        return Collections.singleton(id);
     }
 
     @Override
     public CPropSetElem replaceIdentifier(Identifier source, Identifier target) {
-        return null;
+        return this;
     }
-    */
+
+    @Override
+    public CPropSetElem pushScope(ScopeToken scopeToken, ProgramPoint programPoint) throws SemanticException {
+        return this;
+    }
+
+    @Override
+    public CPropSetElem popScope(ScopeToken scopeToken, ProgramPoint programPoint) throws SemanticException {
+        return this;
+    }
 }

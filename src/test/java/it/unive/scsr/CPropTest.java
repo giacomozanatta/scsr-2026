cb74fe15876422c6b3ab6f3c5da79a1921a4cd24
Daniele Dotto <daniele.dotto@electrolux.com>
2026-03-30 19:38:31 +0200

    test: implemented CPropTest.java in order to test CPropTest.java and visualize the graph

---
+++ src/test/java/it/unive/scsr/CPropTest.java
@@ -0,0 +1,30 @@
+package it.unive.scsr;
+
+import it.unive.lisa.AnalysisException;
+import it.unive.lisa.DefaultConfiguration;
+import it.unive.lisa.LiSA;
+import it.unive.lisa.conf.LiSAConfiguration;
+import it.unive.lisa.imp.IMPFrontend;
+import it.unive.lisa.imp.ParsingException;
+import it.unive.lisa.outputs.HtmlResults;
+import it.unive.lisa.program.Program;
+import it.unive.scsr.analysis.CProp;
+import org.junit.Test;
+
+import static it.unive.lisa.DefaultConfiguration.*;
+
+public class CPropTest {
+
+    @Test
+    public void testCProp() throws ParsingException, AnalysisException {
+        Program program = IMPFrontend.processFile("inputs/cprop.imp");
+
+        LiSAConfiguration conf = new DefaultConfiguration();
+        conf.workdir = "outputs/cprop";
+        conf.outputs.add(new HtmlResults<>(true));
+        conf.analysis = simpleDomain(defaultHeapDomain(), new CProp(), defaultTypeDomain());
+
+        LiSA lisa = new LiSA(conf);
+        lisa.run(program);
+    }
+}

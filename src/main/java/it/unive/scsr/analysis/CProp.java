5672da507c7e2df6273fb37ec92bf869ad214ba3
LucaDM0 <139482590+LucaDM0@users.noreply.github.com>
2026-03-31 13:52:01 +0200

    TASK 2 Added Constant Propagation (CProp) implementation and test

--- src/main/java/it/unive/scsr/analysis/CProp.java
+++ src/main/java/it/unive/scsr/analysis/CProp.java
@@ -1,6 +1,149 @@
 package it.unive.scsr.analysis;
 
-public class CProp /* extends ... */{
+import it.unive.lisa.analysis.dataflow.DataflowDomain;
+import it.unive.lisa.analysis.dataflow.DefiniteSet;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.value.*;
+import it.unive.lisa.symbolic.value.operator.*;
+import it.unive.lisa.symbolic.value.operator.unary.NumericNegation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class CProp extends DataflowDomain<DefiniteSet<CPropSetElem>, CPropSetElem> {
+
+    private static class ParserException extends Exception {
+        public ParserException() {
+        }
+
+        public ParserException(String message) {
+            super(message);
+        }
+    }
+
+    private static final Logger LOG = LogManager.getLogger(CProp.class);
+
+    private static int
+    parserInt(DefiniteSet<CPropSetElem> state, ValueExpression expression)
+            throws NumberFormatException, ParserException {
+        LOG.debug("Processing expression: {} <{}>", expression, expression.getClass().getSimpleName());
+        if (expression instanceof Constant x) {
+            if (x.getValue() instanceof Integer) {
+                return Integer.parseInt(x.getValue().toString());
+            } else {
+                throw new NumberFormatException();
+            }
+        } else if (expression instanceof Identifier x /*Variable or GlobalVariable*/) {
+            if (state.knowsIdentifier(x)) {
+                return state
+                        .getDataflowElements()
+                        .stream()
+                        .filter(cPropSetElem -> cPropSetElem.getId().equals(x))
+                        .findFirst()
+                        .orElseThrow(() -> new ParserException("ERROR: Failed to retrieve constant"))
+                        .getConstant();
+            } else {
+                throw new ParserException("INFO: Expression is not constant");
+            }
+        } else if (expression instanceof UnaryExpression x) {
+            if (x.getOperator() instanceof NumericNegation) {
+                return -(parserInt(state, (ValueExpression) x.getExpression()));
+            }
+        } else if (expression instanceof BinaryExpression x) {
+            ValueExpression l = (ValueExpression) x.getLeft();
+            ValueExpression r = (ValueExpression) x.getRight();
+
+            LOG.info("INFO: Expression is type < {} >", x.getStaticType());
+            if (x.getOperator() instanceof AdditionOperator) {
+                return (parserInt(state, l)) + (parserInt(state, r));
+            }
+            if (x.getOperator() instanceof SubtractionOperator) {
+                return (parserInt(state, l)) - (parserInt(state, r));
+            }
+            if (x.getOperator() instanceof MultiplicationOperator) {
+                return (parserInt(state, l)) * (parserInt(state, r));
+            }
+            if (x.getOperator() instanceof DivisionOperator) {
+                double res = ((double) parserInt(state, l)) / (parserInt(state, r));
+                if (res == (double) (int) res) {
+                    return (int) res;
+                } else {
+                    throw new NumberFormatException();
+                }
+            }
+            if (x.getOperator() instanceof ModuloOperator) {
+                return (parserInt(state, l)) % (parserInt(state, r));
+            }
+        }
+        throw new ParserException("ERROR: Expression not supported");
+    }
+
+    @Override
+    public Set<CPropSetElem> gen(
+            DefiniteSet<CPropSetElem> state,
+            Identifier id,
+            ValueExpression expression,
+            ProgramPoint pp) {
+        Set<CPropSetElem> result = new HashSet<>();
+
+        if (!(id instanceof Variable || id instanceof GlobalVariable)) {
+            return result;
+        }
+
+        Integer val = null;
+        try {
+            val = parserInt(state, expression);
+        } catch (NumberFormatException e) {
+            LOG.error("ERROR: Expression not an integer\n>>> {}", expression.toString());
+        } catch (ParserException u) {
+            LOG.warn("{}\n>>> {}", u.getMessage(), expression.toString());
+        }
+
+        if (val != null) {
+            result.add(new CPropSetElem(id, val));
+        }
+
+        return result;
+    }
+
+    @Override
+    public Set<CPropSetElem> gen(
+            DefiniteSet<CPropSetElem> state,
+            ValueExpression expression,
+            ProgramPoint pp) {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public Set<CPropSetElem> kill(
+            DefiniteSet<CPropSetElem> state,
+            Identifier id,
+            ValueExpression expression,
+            ProgramPoint pp) {
+        return state
+                .getDataflowElements()
+                .stream()
+                .filter(e -> e.getId().equals(id))
+                .collect(Collectors.toCollection(HashSet::new));
+    }
+
+    @Override
+    public Set<CPropSetElem> kill(
+            DefiniteSet<CPropSetElem> state,
+            ValueExpression expression,
+            ProgramPoint pp) {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public DefiniteSet<CPropSetElem> makeLattice() {
+        return new DefiniteSet<>();
+    }
+
 
     // IMPLEMENTATION NOTE:
 

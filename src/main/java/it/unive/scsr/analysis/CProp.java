76b66c2ea900597b05a1d671c0fd2760ab7772f1
Marco <terrinmarco99@gmail.com>
2026-04-01 18:14:55 +0200

    First task

--- src/main/java/it/unive/scsr/analysis/CProp.java
+++ src/main/java/it/unive/scsr/analysis/CProp.java
@@ -1,24 +1,142 @@
 package it.unive.scsr.analysis;
 
-public class CProp /* extends ... */{
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
-    // IMPLEMENTATION NOTE:
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.dataflow.DataflowDomain;
+import it.unive.lisa.analysis.dataflow.DefiniteSet;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.SymbolicExpression;
+import it.unive.lisa.symbolic.value.BinaryExpression;
+import it.unive.lisa.symbolic.value.Constant;
+import it.unive.lisa.symbolic.value.Identifier;
+import it.unive.lisa.symbolic.value.UnaryExpression;
+import it.unive.lisa.symbolic.value.ValueExpression;
+import it.unive.lisa.symbolic.value.operator.AdditionOperator;
+import it.unive.lisa.symbolic.value.operator.DivisionOperator;
+import it.unive.lisa.symbolic.value.operator.MultiplicationOperator;
+import it.unive.lisa.symbolic.value.operator.SubtractionOperator;
+import it.unive.lisa.symbolic.value.operator.binary.BinaryOperator;
+import it.unive.lisa.symbolic.value.operator.unary.NumericNegation;
 
-    // - Implement your solution using the DefiniteSet.
-    //   - What would happen if you used a PossibleSet instead? Think about it (or try it), but remember to deliver the Definite version.
-    // - Keep it simple: track only integer values. Any non-integer values should be ignored.
-    // - To test your implementation, you can use the inputs/cprop.imp file or define your own test cases.
-    // - Refer to the Java test methods discussed in class and adjust them accordingly to work with your domain.
-    // - Constant is subclass of ValueExpression
-    // - How should integer constant values be propagated?
-    //   - Consider the following code snippet:
-    //       1. x = 1
-    //       2. y = x + 2
-    //     The expected output should be:
-    //       1. [x,1]
-    //       2. [x,1] [y,3]
-    //   - How can you retrieve the constant value of `x` to use at program point 2?
-    //   - When working with an object of type `Constant`, you can obtain its value by calling the `getValue()` method.
+public class CProp extends DataflowDomain<DefiniteSet<CPropSetElem>, CPropSetElem>{
 
+    @Override
+    public DefiniteSet<CPropSetElem> makeLattice() {
+        return new DefiniteSet<>();
+    }
 
+    @Override
+    public Set<CPropSetElem> gen(DefiniteSet<CPropSetElem> state, Identifier id, ValueExpression expression,
+            ProgramPoint pp) throws SemanticException {
+
+        Integer value = evalExpression(expression, state);
+        
+        if(value != null) {
+            return Collections.singleton(new CPropSetElem(id, value));
+        }
+
+        return Collections.emptySet();
+    }
+
+    @Override
+    public Set<CPropSetElem> gen(DefiniteSet<CPropSetElem> state, ValueExpression expression, ProgramPoint pp)
+            throws SemanticException {
+        
+        return Collections.emptySet();
+    }
+
+    @Override
+    public Set<CPropSetElem> kill(DefiniteSet<CPropSetElem> state, Identifier id, ValueExpression expression,
+            ProgramPoint pp) throws SemanticException {
+        
+        Set<CPropSetElem> result = new HashSet<>();
+
+		for (CPropSetElem cp : state.getDataflowElements())
+			if (cp.getId().equals(id))
+				result.add(cp);
+
+		return result;
+    }
+
+    @Override
+    public Set<CPropSetElem> kill(DefiniteSet<CPropSetElem> state, ValueExpression expression, ProgramPoint pp)
+            throws SemanticException {
+        
+        return Collections.emptySet();
+    }
+
+    private static Integer getValueOf(
+		Identifier id,
+		DefiniteSet<CPropSetElem> domain) {
+
+		for (CPropSetElem cp : domain.getDataflowElements())
+			if (cp.getId().equals(id))
+				return cp.getConstant();
+		return null;
+	}
+
+    private static Integer evalExpression(SymbolicExpression expr, DefiniteSet<CPropSetElem> domain) {
+        
+        if (expr == null)
+            return null;
+
+        if (expr instanceof Constant constExp) {
+
+            Object value = constExp.getValue();
+    
+            if (value instanceof Integer ris) {
+                return ris;
+            } else {
+                return null;
+            }
+        }
+        if (expr instanceof Identifier expId)
+            return getValueOf(expId, domain);
+
+        if (expr instanceof UnaryExpression unaryExp) {
+            Integer value = evalExpression(unaryExp.getExpression(), domain);
+
+            if (value == null)
+                return null;
+
+            if (unaryExp.getOperator() instanceof NumericNegation)
+                return -value;
+
+            return null;
+        }
+
+        if (expr instanceof BinaryExpression binaryExp) {
+
+            Integer lvalue = evalExpression(binaryExp.getLeft(), domain);
+            Integer rvalue = evalExpression(binaryExp.getRight(), domain);
+            BinaryOperator operator = binaryExp.getOperator();
+
+            if (lvalue == null || rvalue == null)
+                return null;
+
+            if (operator instanceof AdditionOperator)
+                return lvalue + rvalue;
+
+            if (operator instanceof SubtractionOperator)
+                return lvalue - rvalue;
+
+            if (operator instanceof MultiplicationOperator)
+                return lvalue * rvalue;
+
+            if (operator instanceof DivisionOperator) {
+    
+                if (rvalue == 0) {
+                    return null; 
+                }else {
+                    return lvalue / rvalue; 
+                }
+    
+            }
+        }
+
+        return null;
+    }
 }
--- src/main/java/it/unive/scsr/analysis/CPropSetElem.java
+++ src/main/java/it/unive/scsr/analysis/CPropSetElem.java
@@ -1,15 +1,49 @@
 package it.unive.scsr.analysis;
 
-public class CPropSetElem /* implements ... */{
-    /**
-     * IMPLEMENT THIS CLASS
-     * the code below is outside of the scope of the course.
-     * You can uncomment it to get your code to compile.
-     * Be aware that the code is written expecting that a field named "id"
-     * and a field named "constant" exist in this class.
-     */
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
 
-    /*
+import it.unive.lisa.analysis.ScopeToken;
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.dataflow.DataflowElement;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.value.Identifier;
+import it.unive.lisa.util.representation.ListRepresentation;
+import it.unive.lisa.util.representation.StringRepresentation;
+import it.unive.lisa.util.representation.StructuredRepresentation;
+
+public class CPropSetElem implements DataflowElement<CPropSetElem>{
+
+    /**
+    * IMPLEMENT THIS CLASS
+    * the code below is outside of the scope of the course.
+    * You can uncomment it to get your code to compile.
+    * Be aware that the code is written expecting that a field named "id"
+    * and a field named "constant" exist in this class.
+    */
+
+    private final Identifier id;
+    private final Integer constant;
+
+    public CPropSetElem(Identifier id, Integer constant) {
+        this.id = id;
+        this.constant = constant;
+    }
+
+    public Identifier getId() {
+        return this.id;
+    }
+
+    public Integer getConstant() {
+        return this.constant;
+    }
+
+    @Override
+    public Collection<Identifier> getInvolvedIdentifiers() {
+        return Collections.singleton(id);
+    }
+
     @Override
     public StructuredRepresentation representation() {
         return new ListRepresentation(
@@ -18,20 +52,43 @@ public class CPropSetElem /* implements ... */{
     }
 
     @Override
-    public CPropSetElem pushScope(ScopeToken scope)
-            throws SemanticException {
-        return this;
-    }
-
-    @Override
-    public CPropSetElem popScope(ScopeToken scope)
-            throws SemanticException {
-        return this;
-    }
-
-    @Override
     public CPropSetElem replaceIdentifier(Identifier source, Identifier target) {
         return null;
     }
-    */
+
+    @Override
+    public CPropSetElem pushScope(ScopeToken token, ProgramPoint pp) throws SemanticException {
+        return this;
+    }
+
+    @Override
+    public CPropSetElem popScope(ScopeToken token, ProgramPoint pp) throws SemanticException {
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+
+        if (obj == null)
+            return false;
+
+        if(getClass() != obj.getClass())
+            return false;
+
+        CPropSetElem other = (CPropSetElem) obj;
+
+        return this.getId().equals(other.getId()) && this.getConstant().equals(other.getConstant());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getId(), this.getConstant());
+    }
+
+    @Override
+    public String toString() {
+        return this.representation().toString();
+    }
 }

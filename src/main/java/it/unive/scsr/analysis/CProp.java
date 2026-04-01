06e202d295d83cc4887bd9e8a7410a57cab2e44d
PayThePizzo <872966@stud.unive.it>
2026-04-01 11:57:19 +0200

    :sparkles: Built CProp and CPropSetElem classes

--- src/main/java/it/unive/scsr/analysis/CProp.java
+++ src/main/java/it/unive/scsr/analysis/CProp.java
@@ -1,24 +1,265 @@
 package it.unive.scsr.analysis;
 
-public class CProp /* extends ... */{
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.dataflow.DataflowDomain;
+import it.unive.lisa.analysis.dataflow.DefiniteSet;
+import it.unive.lisa.program.cfg.ProgramPoint;
 
-    // IMPLEMENTATION NOTE:
+import it.unive.lisa.symbolic.SymbolicExpression;
+import it.unive.lisa.symbolic.value.UnaryExpression;
+import it.unive.lisa.symbolic.value.BinaryExpression;
+import it.unive.lisa.symbolic.value.TernaryExpression;
 
-    // - Implement your solution using the DefiniteSet.
-    //   - What would happen if you used a PossibleSet instead? Think about it (or try it), but remember to deliver the Definite version.
-    // - Keep it simple: track only integer values. Any non-integer values should be ignored.
-    // - To test your implementation, you can use the inputs/cprop.imp file or define your own test cases.
-    // - Refer to the Java test methods discussed in class and adjust them accordingly to work with your domain.
-    // - Constant is subclass of ValueExpression
-    // - How should integer constant values be propagated?
-    //   - Consider the following code snippet:
-    //       1. x = 1
-    //       2. y = x + 2
-    //     The expected output should be:
-    //       1. [x,1]
-    //       2. [x,1] [y,3]
-    //   - How can you retrieve the constant value of `x` to use at program point 2?
-    //   - When working with an object of type `Constant`, you can obtain its value by calling the `getValue()` method.
+import it.unive.lisa.symbolic.value.Identifier;
+import it.unive.lisa.symbolic.value.ValueExpression;
+import it.unive.lisa.symbolic.value.Constant;
 
+import it.unive.lisa.symbolic.value.operator.*;
+import it.unive.lisa.symbolic.value.operator.unary.UnaryOperator;
+import it.unive.lisa.symbolic.value.operator.binary.BinaryOperator;
+import it.unive.lisa.symbolic.value.operator.ternary.TernaryOperator;
+
+import it.unive.lisa.symbolic.value.operator.unary.NumericNegation;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+
+/**
+ * Class representing the constant propagation analysis domain.
+ * 
+ * @note Docstrings are mostly auto-generated and may need to be revised for clarity and accuracy. This is becaus I had some difficullties to make sense of the surrounding code.
+ * 
+ * @author Gianmaria Pizzo 872966
+ */
+public class CProp extends DataflowDomain<DefiniteSet<CPropSetElem>, CPropSetElem>{
+
+    @Override
+    public DefiniteSet<CPropSetElem> makeLattice() {
+        return new DefiniteSet<>();
+    }
+
+    /**
+     * Returns a collection of identifiers involved in this element.
+     * 
+     * @param id the identifier to be replaced
+     * @param state the current state of the analysis
+     * 
+     * @return the integer value assigned to the identifier in the current state, or null if the identifier is not assigned a constant value.
+     */
+    private static Integer get_value_of(Identifier id, DefiniteSet<CPropSetElem> state) {
+        // Loop through elems
+        for (CPropSetElem cprop_elem : state.getDataflowElements()) {
+            // If id matches
+            if (cprop_elem.getId().equals(id)) {
+                // Return the integer value
+                return cprop_elem.getConstant();
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Extracts the integer value of a given expression based on the current state of the analysis.
+     * 
+     * @param expression the expression to extract the value from
+     * @param state the current state of the analysis
+     * 
+     * @return the integer value of the expression, or null if the expression cannot be evaluated to a constant integer value.
+     */
+    private static Integer extract_expression_value(SymbolicExpression expression, DefiniteSet<CPropSetElem> state) {
+        // Case null expression
+        if (expression == null) {
+            return null;
+        }
+
+        // Case constant expression (ex: 2)
+        if (expression instanceof Constant) {
+            // Get value
+            Object value = ((Constant) expression).getValue();
+
+            // Check if integer
+            if (value instanceof Integer) {
+                return (Integer) value;
+            }
+
+            // Ignore if not integer
+
+        }
+
+        // Case expression is an identifier (ex x;)
+        if (expression instanceof Identifier) {
+            // Find its value and return it (can be null)
+            return get_value_of((Identifier) expression, state);
+        }
+
+        // Case unary expression
+        if (expression instanceof UnaryExpression) {
+            // From unary expression extract the operator and expression
+            UnaryExpression unary_expr = (UnaryExpression) expression;
+            UnaryOperator unary_operator = unary_expr.getOperator();
+            ValueExpression arg = (ValueExpression) unary_expr.getExpression();
+
+            // Extract expression value
+            Integer value = extract_expression_value(arg, state);
+
+            // Base case
+            if (value == null) {
+                return null;
+            }
+
+            // Case "-" operator
+            if (unary_operator instanceof NumericNegation) {
+                // Return negated value
+                return -value;
+            }
+        }
+
+        // Case binary expression
+        if (expression instanceof BinaryExpression) {
+            // Extract binary expression, operator and left/right values
+            BinaryExpression binary_expr = (BinaryExpression) expression;
+            BinaryOperator binary_operator = binary_expr.getOperator();
+
+            Integer left = extract_expression_value(binary_expr.getLeft(), state);
+            Integer right = extract_expression_value(binary_expr.getRight(), state);
+
+            // Base case
+            if (left == null || right == null) {
+                return null;
+            }
+
+            if (binary_operator instanceof AdditionOperator){
+                return left + right;
+            }
+
+            if (binary_operator instanceof SubtractionOperator){
+                return left - right;
+            }
+
+            if (binary_operator instanceof MultiplicationOperator){
+                return left * right;
+            }
+
+            if (binary_operator instanceof DivisionOperator) {
+
+                // Division by zero
+                if (right == 0) {
+                    // Return null
+                    return null;
+                }
+
+                // Enforce integer
+                return ((int) left / right);
+            }
+
+            if (binary_operator instanceof ModuloOperator){
+
+                // Modulo by zero
+                if (right == 0) {
+                    // Return null
+                    return null;
+                }
+
+                // Enforce integer
+                return ((int) left % right);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Generates a set of elements based on the given state and expression.
+     *
+     * @param state the current state of the analysis
+     * @param id the identifier to be replaced
+     * @param expression the expression to extract the value from
+     * @param pp the program point
+     * 
+     * @return the set of generated elements
+     * 
+     * @throws SemanticException if a semantic error occurs
+     */
+    @Override
+    public Set<CPropSetElem> gen(DefiniteSet<CPropSetElem> state, Identifier id, ValueExpression expression, ProgramPoint pp) throws SemanticException {
+        // Build new gen set
+        Set<CPropSetElem> generated = new HashSet<>();
+
+        // extract the expression's value as an integer (hopefully)
+        Integer value = extract_expression_value(expression, state);
+
+        // If the val is not null
+        if (value != null){
+            // Add new element of the set with the id and the found integer value
+            generated.add(new CPropSetElem(id, value));
+        }
+
+        // Returns either empty set or with new element
+        return generated;
+    }
+
+    /**
+     * Generates a set of elements based on the given state and expression.
+     *
+     * @note This method is not used in the current implementation of the analysis, but it is required by the interface. It returns an empty set by default.
+     * 
+     * @param state the current state of the analysis
+     * @param expression the expression to extract the value from
+     * @param pp the program point
+     * 
+     * @return the set of generated elements
+     * 
+     * @throws SemanticException if a semantic error occurs
+     */
+    @Override
+    public Set<CPropSetElem> gen(DefiniteSet<CPropSetElem> state, ValueExpression expression, ProgramPoint pp) throws SemanticException {
+        // Missing id, return empty set by default
+        return Collections.emptySet();
+    }
+
+    /**
+     * Replaces an identifier with another in this element.
+     * 
+     * @param source the identifier to be replaced
+     * @param target the identifier to replace with
+     * 
+     * @return a new element with the identifier replaced, or null if the replacement is not applicable.
+     */
+    @Override
+    public Set<CPropSetElem> kill(DefiniteSet<CPropSetElem> state, Identifier id, ValueExpression expression, ProgramPoint pp) throws SemanticException {
+        // Build a new set
+        Set<CPropSetElem> killed = new HashSet<>();
+
+        // Loop through the elements
+        for (CPropSetElem cprop_elem : state.getDataflowElements()) {
+            // If the elem id matches the given id
+            if (cprop_elem.getId().equals(id)) {
+                // Insert in killed set
+                killed.add(cprop_elem);
+            }
+        }
+
+        return killed;
+    }
+
+    /**
+     * Replaces an identifier with another in this element.
+     * 
+     * @note This method is not used in the current implementation of the analysis, but it is required by the interface. It returns an empty set by default.
+     * 
+     * @param source the identifier to be replaced
+     * @param target the identifier to replace with
+     * 
+     * @return a new element with the identifier replaced, or null if the replacement is not applicable.
+     */
+    @Override
+    public Set<CPropSetElem> kill(DefiniteSet<CPropSetElem> state, ValueExpression expression, ProgramPoint pp) throws SemanticException {
+        // Missing id, return empty set by default
+        return Collections.emptySet();
+    }
 
 }
--- src/main/java/it/unive/scsr/analysis/CPropSetElem.java
+++ src/main/java/it/unive/scsr/analysis/CPropSetElem.java
@@ -1,15 +1,65 @@
 package it.unive.scsr.analysis;
 
-public class CPropSetElem /* implements ... */{
-    /**
-     * IMPLEMENT THIS CLASS
-     * the code below is outside of the scope of the course.
-     * You can uncomment it to get your code to compile.
-     * Be aware that the code is written expecting that a field named "id"
-     * and a field named "constant" exist in this class.
-     */
+import it.unive.lisa.analysis.ScopeToken;
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.dataflow.DataflowElement;
+import it.unive.lisa.program.cfg.ProgramPoint;
 
-    /*
+import it.unive.lisa.symbolic.value.Identifier;
+import it.unive.lisa.symbolic.value.ValueExpression;
+import it.unive.lisa.symbolic.value.Constant;
+
+import it.unive.lisa.util.representation.ListRepresentation;
+import it.unive.lisa.util.representation.StringRepresentation;
+import it.unive.lisa.util.representation.StructuredRepresentation;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+
+
+/**
+ * Class representing an element of the set of the constant propagation analysis.
+ *
+ * @note Docstrings are mostly auto-generated and may need to be revised for clarity and accuracy. This is becaus I had some difficullties to make sense of the surrounding code.
+ * 
+ * @author Gianmaria Pizzo 872966
+ */
+public class CPropSetElem implements DataflowElement<CPropSetElem>{
+    // ================================================================
+    // Fields
+
+    private final Identifier id;        /// Identifier
+    private final Integer constant;     /// Integer Constant
+
+    // ================================================================
+    // Constructors
+
+    /**
+     * Constructs a new Constant Propagation element with null values.
+     * 
+     */
+    public CPropSetElem() {
+        this.id = null;
+        this.constant = null;
+    }
+
+    /**
+     * Constructs a new Constant Propagation element.
+     *
+     * @param id the identifier
+     * @param constant the constant integer value assigned to the identifier
+     *
+     */
+    public CPropSetElem(Identifier id, Integer constant) {
+        super();
+        this.id = id;
+        this.constant = constant;
+    }
+
+    // ================================================================
+    // Given Methods (Untouched)
+
     @Override
     public StructuredRepresentation representation() {
         return new ListRepresentation(
@@ -17,21 +67,105 @@ public class CPropSetElem /* implements ... */{
                 new StringRepresentation(constant));
     }
 
+    // NOTE: I noticed the task template for pushScope does not match the current ScopedObject implementation.
+    //  Therefore, I just changed the method signature.
+
     @Override
-    public CPropSetElem pushScope(ScopeToken scope)
-            throws SemanticException {
+    public CPropSetElem pushScope(ScopeToken token, ProgramPoint pp) throws SemanticException {
         return this;
     }
 
+    // NOTE: I noticed the task template for popScope does not match the current ScopedObject implementation.
+    //  Therefore, I just changed the method signature.
+
     @Override
-    public CPropSetElem popScope(ScopeToken scope)
-            throws SemanticException {
+    public CPropSetElem popScope(ScopeToken token, ProgramPoint pp) throws SemanticException {
         return this;
     }
 
+    // ================================================================
+    // Methods implemented for Constant Propagation Task
+
+    /**
+     * Replaces an identifier with another in this element.
+     * 
+     * @param source the identifier to be replaced
+     * @param target the identifier to replace with
+     * 
+     * @return a new element with the identifier replaced, or null if the replacement is not applicable.
+     */
     @Override
     public CPropSetElem replaceIdentifier(Identifier source, Identifier target) {
-        return null;
+        return null;    // As for AvailableExpressionSetElem and ReachingDefinitionSet.
     }
-    */
+
+    /**
+     * Returns a collection of identifiers involved in this element.
+     * 
+     * @return a collection of identifiers involved in this element
+     */
+    @Override
+    public Collection<Identifier> getInvolvedIdentifiers() {
+        return Collections.singleton(id);
+    }
+
+    /**
+     * Getter for the identifier of this element.
+     * 
+     * @return the identifier of this element
+     */
+    public Identifier getId(){
+        return id;
+    }
+
+    /**
+     * Getter for the constant value of this element.
+     * 
+     * @return the constant value of this element
+     */
+    public Integer getConstant(){
+        return constant;
+    }
+
+    /**
+     * Returns a string representation of this element.
+     * 
+     * @return a string representation of this element
+     */
+    @Override
+    public String toString() {
+        return representation().toString();
+    }
+
+    /**
+     * Checks if this element is equal to another object.
+     * 
+     * @param o the object to compare with
+     * @return true if the objects are equal, false otherwise
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o){
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()){
+            return false;
+        }
+
+        CPropSetElem that = (CPropSetElem) o;
+
+        return Objects.equals(id, that.id) && Objects.equals(constant, that.constant);
+    }
+
+    /**
+     * Returns the hash code of this element.
+     * 
+     * @return the hash code of this element
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, constant);
+    }
+
 }

dc338bcc950e13750a877e2dffed6f5b7da9f53a
Giacomo Zanatta <giacomo@Giacomos-MacBook-Pro.local>
2026-04-07 16:01:43 +0200

    Add evaluation

--- src/main/java/it/unive/scsr/analysis/CPropSetElem.java
+++ src/main/java/it/unive/scsr/analysis/CPropSetElem.java
@@ -1,37 +1,74 @@
 package it.unive.scsr.analysis;
+import it.unive.lisa.analysis.ScopeToken;
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.dataflow.DataflowElement;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.value.*;
+import it.unive.lisa.util.representation.ListRepresentation;
+import it.unive.lisa.util.representation.StringRepresentation;
+import it.unive.lisa.util.representation.StructuredRepresentation;
 
-public class CPropSetElem /* implements ... */{
-    /**
-     * IMPLEMENT THIS CLASS
-     * the code below is outside of the scope of the course.
-     * You can uncomment it to get your code to compile.
-     * Be aware that the code is written expecting that a field named "id"
-     * and a field named "constant" exist in this class.
-     */
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
 
-    /*
+public class CPropSetElem  implements DataflowElement<CPropSetElem> {
+    private final Identifier id;
+    private final Integer constant;
+
+    public CPropSetElem(Identifier id, Integer constant) {
+        this.id = id;
+        this.constant = constant;
+    }
+
     @Override
     public StructuredRepresentation representation() {
         return new ListRepresentation(
-                new StringRepresentation(id),
-                new StringRepresentation(constant));
+                new StringRepresentation(this.id),
+                new StringRepresentation(this.constant));
     }
 
     @Override
-    public CPropSetElem pushScope(ScopeToken scope)
-            throws SemanticException {
-        return this;
-    }
-
-    @Override
-    public CPropSetElem popScope(ScopeToken scope)
-            throws SemanticException {
-        return this;
+    public Collection<Identifier> getInvolvedIdentifiers() {
+        return Collections.singleton(this.id);
     }
 
     @Override
     public CPropSetElem replaceIdentifier(Identifier source, Identifier target) {
-        return null;
+        // Single element, we can just create a new one and be happy.
+        return new CPropSetElem(target, this.constant);
     }
-    */
+
+    @Override
+    public CPropSetElem pushScope(ScopeToken token, ProgramPoint pp) throws SemanticException {
+        return this;
+    }
+
+    @Override
+    public CPropSetElem popScope(ScopeToken token, ProgramPoint pp) throws SemanticException {
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return this.representation().toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+        CPropSetElem rhs = (CPropSetElem) o;
+        return Objects.equals(this.id, rhs.id) && Objects.equals(this.constant, rhs.constant);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.constant);
+    }
+
+    // I need this for the case y = x and x is already saved with a constant
+    public Integer getConstant() {
+        return this.constant;
+    }
 }

695b287c58ce05c15505fc145bc86c29bcfca167
mattiaAcqui <mattiacquila03@gmail.com>
2026-04-11 17:13:40 +0200

    Implemented the non-relational abstract domain parity

--- src/main/java/it/unive/scsr/analysis/parity/Parity.java
+++ src/main/java/it/unive/scsr/analysis/parity/Parity.java
@@ -1,21 +1,109 @@
 package it.unive.scsr.analysis.parity;
 
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.SemanticOracle;
 import it.unive.lisa.analysis.nonrelational.value.BaseNonRelationalValueDomain;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.value.BinaryExpression;
+import it.unive.lisa.symbolic.value.Constant;
+import it.unive.lisa.symbolic.value.UnaryExpression;
+import it.unive.lisa.symbolic.value.operator.*;
+import it.unive.lisa.symbolic.value.operator.unary.NumericNegation;
 
-public class Parity implements
-		BaseNonRelationalValueDomain<ParityLattice> {
+
+/**
+ * @author Mattia Acquilesi 896827
+ * @author Alan Dal Col 895879
+ */
+public class Parity implements BaseNonRelationalValueDomain<ParityLattice> {
 
 	@Override
-	    public ParityLattice top() {
-			// TODO: homework
-			return null;
-	    }
+	public ParityLattice top() {
+		return ParityLattice.TOP;
+	}
 
-	    @Override
-	    public ParityLattice bottom() {
-	    	// TODO: homework
-			return null;
-	    }
+	@Override
+	public ParityLattice bottom() {
+		return ParityLattice.BOTTOM;
+	}
 
-	    //TODO add missing components
+	@Override
+	public ParityLattice evalConstant(
+			Constant constant,
+			ProgramPoint pp,
+			SemanticOracle oracle) throws SemanticException {
+
+		if (constant.getValue() instanceof Integer n) {
+            return (n % 2 == 0) ? ParityLattice.EVEN : ParityLattice.ODD;
+		}
+
+		return ParityLattice.TOP;
+	}
+
+	@Override
+	public ParityLattice evalUnaryExpression(
+			UnaryExpression expression,
+			ParityLattice arg,
+			ProgramPoint pp,
+			SemanticOracle oracle) throws SemanticException {
+
+		if (expression.getOperator() == NumericNegation.INSTANCE) {
+			return arg;
+		}
+		return ParityLattice.TOP;
+	}
+
+	@Override
+	public ParityLattice evalBinaryExpression(
+			BinaryExpression expression,
+			ParityLattice left,
+			ParityLattice right,
+			ProgramPoint pp,
+			SemanticOracle oracle) throws SemanticException {
+
+		if (left == ParityLattice.BOTTOM || right == ParityLattice.BOTTOM)
+			return ParityLattice.BOTTOM;
+
+		if (expression.getOperator() instanceof AdditionOperator
+				|| expression.getOperator() instanceof SubtractionOperator) {
+			return parityAdd(left, right);
+		}
+
+		if (expression.getOperator() instanceof MultiplicationOperator) {
+			return parityMul(left, right);
+		}
+
+		if (expression.getOperator() instanceof DivisionOperator) {
+			return ParityLattice.TOP;
+		}
+
+		if (expression.getOperator() instanceof ModuloOperator
+				|| expression.getOperator() instanceof RemainderOperator) {
+            if (left == ParityLattice.EVEN && right == ParityLattice.EVEN)
+				return ParityLattice.EVEN;
+			if (left == ParityLattice.ODD && right == ParityLattice.EVEN)
+				return ParityLattice.ODD;
+			return ParityLattice.TOP;
+		}
+
+		return ParityLattice.TOP;
+	}
+
+
+	private ParityLattice parityAdd(ParityLattice left, ParityLattice right) {
+		if (left == ParityLattice.TOP || right == ParityLattice.TOP)
+			return ParityLattice.TOP;
+		if (left == right)
+			return ParityLattice.EVEN;
+		return ParityLattice.ODD;
+	}
+
+
+	private ParityLattice parityMul(ParityLattice left, ParityLattice right) {
+		if (left == ParityLattice.EVEN || right == ParityLattice.EVEN)
+			return ParityLattice.EVEN;
+		if (left == ParityLattice.TOP || right == ParityLattice.TOP)
+			return ParityLattice.TOP;
+		return ParityLattice.ODD;
+	}
 }
--- src/main/java/it/unive/scsr/analysis/parity/ParityLattice.java
+++ src/main/java/it/unive/scsr/analysis/parity/ParityLattice.java
@@ -1,41 +1,71 @@
 package it.unive.scsr.analysis.parity;
 
 import it.unive.lisa.analysis.BaseLattice;
+import it.unive.lisa.analysis.Lattice;
 import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.util.representation.StringRepresentation;
 import it.unive.lisa.util.representation.StructuredRepresentation;
 
-//Homework
+import java.util.Objects;
+
+/**
+ * @author Mattia Acquilesi 896827
+ * @author Alan Dal Col 895879
+ */
 public class ParityLattice implements BaseLattice<ParityLattice> {
+
+	private final int element;
+
+	// Encoding: 0=TOP, 1=EVEN, 2=ODD, 3=BOTTOM
+	public static final ParityLattice TOP    = new ParityLattice(0);
+	public static final ParityLattice EVEN   = new ParityLattice(1);
+	public static final ParityLattice ODD    = new ParityLattice(2);
+	public static final ParityLattice BOTTOM = new ParityLattice(3);
+
+	public ParityLattice(int e) {
+		this.element = e;
+	}
 
 	@Override
 	public ParityLattice top() {
-		// TODO: homework
-		return null;
+		return TOP;
 	}
 
 	@Override
 	public ParityLattice bottom() {
-		// TODO: homework
-		return null;
+		return BOTTOM;
 	}
 
+
 	@Override
-	public StructuredRepresentation representation() {
-		// TODO: homework
-		return null;
+	public boolean lessOrEqualAux(ParityLattice other) throws SemanticException {
+		return false;
 	}
 
 	@Override
 	public ParityLattice lubAux(ParityLattice other) throws SemanticException {
-		// TODO: homework
-		return null;
+		return TOP;
+	}
+
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		ParityLattice that = (ParityLattice) o;
+		return element == that.element;
 	}
 
 	@Override
-	public boolean lessOrEqualAux(ParityLattice other) throws SemanticException {
-		// TODO: homework
-		return false;
+	public int hashCode() {
+		return Objects.hash(element);
 	}
 
-	// TODO add missing components
+	@Override
+	public StructuredRepresentation representation() {
+		if (this == BOTTOM) return Lattice.bottomRepresentation();
+		if (this == TOP)    return Lattice.topRepresentation();
+		if (this == EVEN)   return new StringRepresentation("EVEN");
+		return new StringRepresentation("ODD");
+	}
 }

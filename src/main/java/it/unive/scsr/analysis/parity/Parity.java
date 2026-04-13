504a16b612383674daf7281db9ce0277f5d032f1
LucaDM0 <139482590+LucaDM0@users.noreply.github.com>
2026-04-13 12:27:00 +0200

    TASK 3 Implemented Parity and ParityLattice with test

--- src/main/java/it/unive/scsr/analysis/parity/Parity.java
+++ src/main/java/it/unive/scsr/analysis/parity/Parity.java
@@ -1,21 +1,108 @@
 package it.unive.scsr.analysis.parity;
 
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.SemanticOracle;
 import it.unive.lisa.analysis.nonrelational.value.BaseNonRelationalValueDomain;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.value.BinaryExpression;
+import it.unive.lisa.symbolic.value.Constant;
+import it.unive.lisa.symbolic.value.UnaryExpression;
+import it.unive.lisa.symbolic.value.operator.*;
+import it.unive.lisa.symbolic.value.operator.unary.NumericAbs;
+import it.unive.lisa.symbolic.value.operator.unary.NumericNegation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class Parity implements
-		BaseNonRelationalValueDomain<ParityLattice> {
+public class Parity implements BaseNonRelationalValueDomain<ParityLattice> {
+
+	private static final Logger LOG = LogManager.getLogger(Parity.class);
 
 	@Override
-	    public ParityLattice top() {
-			// TODO: homework
-			return null;
-	    }
+	public ParityLattice top() {
+		return ParityLattice.TOP;
+	}
 
-	    @Override
-	    public ParityLattice bottom() {
-	    	// TODO: homework
-			return null;
-	    }
+	@Override
+	public ParityLattice bottom() {
+		return ParityLattice.BOTTOM;
+	}
 
-	    //TODO add missing components
+	@Override
+	public ParityLattice
+	evalConstant(Constant constant, ProgramPoint pp, SemanticOracle oracle) throws SemanticException {
+		if (constant.getValue() instanceof Integer n) {
+			if (n % 2 == 0)
+				return ParityLattice.EVEN;
+			return ParityLattice.ODD;
+		}
+		return ParityLattice.TOP;
+	}
+
+	@Override
+	public ParityLattice evalUnaryExpression(
+			UnaryExpression expression,
+			ParityLattice arg,
+			ProgramPoint pp,
+			SemanticOracle oracle) throws SemanticException {
+		if (expression.getOperator() == NumericNegation.INSTANCE)
+			return arg;
+		else if (expression.getOperator() == NumericAbs.INSTANCE)
+			return arg;
+		return ParityLattice.TOP;
+	}
+
+	@Override
+	public ParityLattice evalBinaryExpression(
+			BinaryExpression expression,
+			ParityLattice left,
+			ParityLattice right,
+			ProgramPoint pp,
+			SemanticOracle oracle) throws SemanticException {
+
+		// Probably unnecessary but not sure
+		if (left == null || right == null)
+			return ParityLattice.BOTTOM;
+
+		// TOP propagation
+		if (left.isTop() || right.isTop())
+			return ParityLattice.TOP;
+
+		// Redundant, handled by parent class
+		//if (left.isBottom() || right.isBottom())
+		//	return ParityLattice.BOTTOM;
+
+		if (expression.getOperator() instanceof AdditionOperator) {
+			// Same as SubtractionOperator
+			if (left == right)
+				return ParityLattice.EVEN;
+			else
+				return ParityLattice.ODD;
+		} else if (expression.getOperator() instanceof MultiplicationOperator) {
+			if (left == ParityLattice.EVEN || right == ParityLattice.EVEN)
+				return ParityLattice.EVEN;
+			else
+				return ParityLattice.ODD;
+		} else if (expression.getOperator() instanceof SubtractionOperator) {
+			// Same as AdditionOperator
+			if (left == right)
+				return ParityLattice.EVEN;
+			else
+				return ParityLattice.ODD;
+		} else if (expression.getOperator() instanceof DivisionOperator) {
+			return ParityLattice.TOP;
+		} else if (expression.getOperator() instanceof ModuloOperator) {
+			// Same as RemainderOperator
+			if (right == ParityLattice.EVEN)
+				return left;
+			return ParityLattice.TOP;
+		} else if (expression.getOperator() instanceof RemainderOperator) {
+			// Same as ModuloOperator
+			if (right == ParityLattice.EVEN)
+				return left;
+			return ParityLattice.TOP;
+		}
+
+		return ParityLattice.TOP;
+	}
+
 }
--- src/main/java/it/unive/scsr/analysis/parity/ParityLattice.java
+++ src/main/java/it/unive/scsr/analysis/parity/ParityLattice.java
@@ -1,41 +1,89 @@
 package it.unive.scsr.analysis.parity;
 
 import it.unive.lisa.analysis.BaseLattice;
+import it.unive.lisa.analysis.Lattice;
 import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.util.representation.StringRepresentation;
 import it.unive.lisa.util.representation.StructuredRepresentation;
+
+import java.util.Objects;
 
 //Homework
 public class ParityLattice implements BaseLattice<ParityLattice> {
 
+	private final int element;
+
+	public static ParityLattice TOP = new ParityLattice(0);
+	public static ParityLattice ODD = new ParityLattice(1);
+	public static ParityLattice EVEN = new ParityLattice(2);
+	public static ParityLattice BOTTOM = new ParityLattice(3);
+
+	public ParityLattice(int e) {
+		element = e;
+	}
+
 	@Override
 	public ParityLattice top() {
-		// TODO: homework
-		return null;
+		return ParityLattice.TOP;
 	}
 
 	@Override
 	public ParityLattice bottom() {
-		// TODO: homework
-		return null;
+		return ParityLattice.BOTTOM;
 	}
 
 	@Override
 	public StructuredRepresentation representation() {
-		// TODO: homework
-		return null;
+		if(this == ParityLattice.BOTTOM)
+			return Lattice.bottomRepresentation();
+		else if(this == ParityLattice.TOP)
+			return Lattice.topRepresentation();
+		else if(this == ParityLattice.EVEN)
+			return new StringRepresentation("Even");
+		else
+			return new StringRepresentation("Odd");
 	}
 
 	@Override
 	public ParityLattice lubAux(ParityLattice other) throws SemanticException {
-		// TODO: homework
-		return null;
+		/*
+		// Base cases are handled by parent class
+		if ( this.isTop() || other.isTop())
+			return ParityLattice.TOP;
+		else if (this.isBottom())
+			return other;
+		else if (other.isBottom())
+			return this;
+		else if (this == other)
+			return this;
+		 */
+		return ParityLattice.TOP;
 	}
 
 	@Override
 	public boolean lessOrEqualAux(ParityLattice other) throws SemanticException {
-		// TODO: homework
+		/*
+		// Base cases are handled by parent class
+		if (this == other)
+			return true;
+		else if (this.isBottom())
+			return true;
+		else if (other.isTop())
+			return true;
+		 */
+
 		return false;
 	}
 
-	// TODO add missing components
+	@Override
+	public boolean equals(Object o) {
+		if (o == null || getClass() != o.getClass()) return false;
+		ParityLattice that = (ParityLattice) o;
+		return element == that.element;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(element);
+	}
 }

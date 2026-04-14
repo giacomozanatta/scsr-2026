af2e9f2300313edcc6f2a617c6ffe1806fa8c75e
Tommaso Soncin <tommaso.soncin@protonmail.com>
2026-04-14 18:50:56 +0200

    feat: Lab 3 done
    Might want to check it before 17th of April

--- src/main/java/it/unive/scsr/analysis/parity/Parity.java
+++ src/main/java/it/unive/scsr/analysis/parity/Parity.java
@@ -1,21 +1,96 @@
 package it.unive.scsr.analysis.parity;
 
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.SemanticOracle;
 import it.unive.lisa.analysis.nonrelational.value.BaseNonRelationalValueDomain;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.value.BinaryExpression;
+import it.unive.lisa.symbolic.value.Constant;
+import it.unive.lisa.symbolic.value.UnaryExpression;
+import it.unive.lisa.symbolic.value.operator.*;
+import it.unive.lisa.symbolic.value.operator.unary.NumericNegation;
+import it.unive.lisa.symbolic.value.operator.unary.UnaryOperator;
 
 public class Parity implements
-		BaseNonRelationalValueDomain<ParityLattice> {
+        BaseNonRelationalValueDomain<ParityLattice> {
 
-	@Override
-	    public ParityLattice top() {
-			// TODO: homework
-			return null;
-	    }
+    @Override
+    public ParityLattice top() {
+        return ParityLattice.TOP;
+    }
 
-	    @Override
-	    public ParityLattice bottom() {
-	    	// TODO: homework
-			return null;
-	    }
+    @Override
+    public ParityLattice bottom() {
+        return ParityLattice.BOTTOM;
+    }
 
-	    //TODO add missing components
+    //TODO add missing components
+    @Override
+    public ParityLattice evalConstant(Constant constant, ProgramPoint pp, SemanticOracle oracle) throws SemanticException {
+        if (constant.getValue() instanceof Integer value) {
+            return value % 2 == 0 ? ParityLattice.EVEN : ParityLattice.ODD;
+        }
+        return ParityLattice.TOP;
+    }
+
+    @Override
+    public ParityLattice evalUnaryExpression(UnaryExpression expression, ParityLattice arg, ProgramPoint pp,
+                                             SemanticOracle oracle) throws SemanticException {
+
+
+        // One operand -> -
+        // I should always know if - <some_value> is even or odd
+        if (expression.getOperator() instanceof NumericNegation)
+            return arg;
+        // anything else
+        return ParityLattice.TOP;
+    }
+
+    @Override
+    public ParityLattice evalBinaryExpression(BinaryExpression expression, ParityLattice left, ParityLattice right,
+                                              ProgramPoint pp, SemanticOracle oracle) throws SemanticException {
+
+        if (left.isTop() || right.isTop())
+            return ParityLattice.TOP;
+        if (left.isBottom() || right.isBottom())
+            return ParityLattice.BOTTOM;
+
+        // Two operand -> +-*/
+        if (expression.getOperator() instanceof AdditionOperator) {
+            if (left == ParityLattice.EVEN && right == ParityLattice.EVEN)
+                return ParityLattice.EVEN;
+            if (left == ParityLattice.EVEN && right == ParityLattice.ODD)
+                return ParityLattice.ODD;
+            if (left == ParityLattice.ODD && right == ParityLattice.EVEN)
+                return ParityLattice.ODD;
+            if (left == ParityLattice.ODD && right == ParityLattice.ODD)
+                return ParityLattice.EVEN;
+        }
+        if (expression.getOperator() instanceof SubtractionOperator) {
+            if (left == ParityLattice.EVEN && right == ParityLattice.EVEN)
+                return ParityLattice.EVEN;
+            if (left == ParityLattice.EVEN && right == ParityLattice.ODD)
+                return ParityLattice.ODD;
+            if (left == ParityLattice.ODD && right == ParityLattice.EVEN)
+                return ParityLattice.ODD;
+            if (left == ParityLattice.ODD && right == ParityLattice.ODD)
+                return ParityLattice.EVEN;
+        }
+        if (expression.getOperator() instanceof MultiplicationOperator) {
+            if (left == ParityLattice.EVEN && right == ParityLattice.EVEN)
+                return ParityLattice.EVEN;
+            if (left == ParityLattice.EVEN && right == ParityLattice.ODD)
+                return ParityLattice.EVEN;
+            if (left == ParityLattice.ODD && right == ParityLattice.EVEN)
+                return ParityLattice.EVEN;
+            if (left == ParityLattice.ODD && right == ParityLattice.ODD)
+                return ParityLattice.ODD;
+        }
+        if (expression.getOperator() instanceof DivisionOperator) {
+            return ParityLattice.TOP; // we can never be sure (IntelliJ screams about deleting it, I don't want)
+        }
+
+        return ParityLattice.TOP;
+    }
+
 }
--- src/main/java/it/unive/scsr/analysis/parity/ParityLattice.java
+++ src/main/java/it/unive/scsr/analysis/parity/ParityLattice.java
@@ -2,40 +2,97 @@ package it.unive.scsr.analysis.parity;
 
 import it.unive.lisa.analysis.BaseLattice;
 import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.lattices.Satisfiability;
+import it.unive.lisa.util.representation.StringRepresentation;
 import it.unive.lisa.util.representation.StructuredRepresentation;
+import it.unive.scsr.analysis.sign.SignLattice;
+
+import java.util.Objects;
 
 //Homework
 public class ParityLattice implements BaseLattice<ParityLattice> {
+    private final String parity;
 
-	@Override
-	public ParityLattice top() {
-		// TODO: homework
-		return null;
-	}
+    public static ParityLattice TOP = new ParityLattice("TOP");
+    public static ParityLattice BOTTOM = new ParityLattice("BOTTOM");
+    public static ParityLattice EVEN = new ParityLattice("EVEN");
+    public static ParityLattice ODD = new ParityLattice("ODD");
 
-	@Override
-	public ParityLattice bottom() {
-		// TODO: homework
-		return null;
-	}
+    public ParityLattice() {
+        this.parity = "TOP";
+    }
 
-	@Override
-	public StructuredRepresentation representation() {
-		// TODO: homework
-		return null;
-	}
+    public ParityLattice(String parity) {
+        this.parity = parity;
+    }
 
-	@Override
-	public ParityLattice lubAux(ParityLattice other) throws SemanticException {
-		// TODO: homework
-		return null;
-	}
+    @Override
+    public int hashCode() {
+        return parity.hashCode();
+    }
 
-	@Override
-	public boolean lessOrEqualAux(ParityLattice other) throws SemanticException {
-		// TODO: homework
-		return false;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (this.getClass() != obj.getClass())
+            return false;
+        ParityLattice other = (ParityLattice) obj;
+        return Objects.equals(this.parity, other.parity);
+    }
 
-	// TODO add missing components
+    @Override
+    public ParityLattice top() {
+        return ParityLattice.TOP;
+    }
+
+    @Override
+    public ParityLattice bottom() {
+        return ParityLattice.BOTTOM;
+    }
+
+    @Override
+    public StructuredRepresentation representation() {
+        return new StringRepresentation(parity);
+    }
+
+    @Override
+    public ParityLattice lubAux(ParityLattice other) throws SemanticException {
+        return ParityLattice.TOP;
+    }
+
+    @Override
+    public boolean lessOrEqualAux(ParityLattice other) throws SemanticException {
+        return false;
+    }
+
+    // almost copying SignLattice
+    // For glb in this case we use default LiSA implementation
+    // Other method implementations to use in Parity domain to assume and check satisfability of some stuff
+    public Satisfiability eq(
+            ParityLattice other) {
+        if (this.isBottom() || other.isBottom())
+            return Satisfiability.BOTTOM;
+        else if (this.isTop() || other.isTop())
+            return Satisfiability.UNKNOWN;
+        else if (!this.equals(other))
+            return Satisfiability.NOT_SATISFIED;
+        return Satisfiability.UNKNOWN;
+    }
+
+    /**
+     * Tests if this instance is greater than the given one, returning a
+     * {@link Satisfiability} element.
+     *
+     * @param other the instance
+     * @return the satisfiability of {@code this > other}
+     */
+    public Satisfiability gt(
+            ParityLattice other) {
+        if (this.isBottom() || other.isBottom())
+            return Satisfiability.BOTTOM;
+        return Satisfiability.UNKNOWN; // depends on actual values, an even number can be bigger than an odd number and vice versa
+    }
 }

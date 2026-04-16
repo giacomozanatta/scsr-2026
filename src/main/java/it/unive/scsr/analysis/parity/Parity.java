ee4cbb0961b546b5fba8e177f123338b191db2bf
forsinu <887708@stud.unive.it>
2026-04-16 19:13:05 +0200

    [TASK 2] Parity Abstract Domain Implementation

--- src/main/java/it/unive/scsr/analysis/parity/Parity.java
+++ src/main/java/it/unive/scsr/analysis/parity/Parity.java
@@ -1,21 +1,167 @@
 package it.unive.scsr.analysis.parity;
 
+import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.analysis.SemanticOracle;
 import it.unive.lisa.analysis.nonrelational.value.BaseNonRelationalValueDomain;
+import it.unive.lisa.program.cfg.ProgramPoint;
+import it.unive.lisa.symbolic.value.BinaryExpression;
+import it.unive.lisa.symbolic.value.Constant;
+import it.unive.lisa.symbolic.value.UnaryExpression;
+import it.unive.lisa.symbolic.value.operator.AdditionOperator;
+import it.unive.lisa.symbolic.value.operator.ModuloOperator;
+import it.unive.lisa.symbolic.value.operator.MultiplicationOperator;
+import it.unive.lisa.symbolic.value.operator.RemainderOperator;
+import it.unive.lisa.symbolic.value.operator.SubtractionOperator;
+import it.unive.lisa.symbolic.value.operator.binary.BinaryOperator;
+import it.unive.lisa.symbolic.value.operator.unary.NumericNegation;
 
 public class Parity implements
 		BaseNonRelationalValueDomain<ParityLattice> {
 
 	@Override
-	    public ParityLattice top() {
-			// TODO: homework
-			return null;
-	    }
+	public ParityLattice top() {
+		return ParityLattice.TOP;
+	}
 
-	    @Override
-	    public ParityLattice bottom() {
-	    	// TODO: homework
-			return null;
-	    }
+	@Override
+	public ParityLattice bottom() {
+		return ParityLattice.BOTTOM;
+	}
 
-	    //TODO add missing components
+	@Override
+	public ParityLattice evalConstant(Constant constant, ProgramPoint pp, SemanticOracle oracle)
+			throws SemanticException {
+
+		if (constant.getValue() instanceof Integer val)
+			return ((val & 1) == 0) ? ParityLattice.EVEN : ParityLattice.ODD;
+
+		return top();
+	}
+
+	@Override
+	public ParityLattice evalUnaryExpression(
+			UnaryExpression expression,
+			ParityLattice arg,
+			ProgramPoint pp,
+			SemanticOracle oracle) throws SemanticException {
+
+		return (expression.getOperator() instanceof NumericNegation) ? arg : top();
+	}
+
+	@Override
+	public ParityLattice evalBinaryExpression(BinaryExpression expression, ParityLattice left, ParityLattice right,
+			ProgramPoint pp, SemanticOracle oracle) throws SemanticException {
+
+		if (left == ParityLattice.BOTTOM || right == ParityLattice.BOTTOM)
+			return bottom();
+
+		BinaryOperator op = expression.getOperator();
+
+		if (op instanceof AdditionOperator || op instanceof SubtractionOperator) {
+			if (left == ParityLattice.EVEN)
+				return right;
+
+			if (left == ParityLattice.ODD) {
+				if (right == ParityLattice.ODD)
+					return ParityLattice.EVEN;
+
+				if (right == ParityLattice.EVEN)
+					return ParityLattice.ODD;
+			}
+		}
+
+		if (op instanceof MultiplicationOperator) {
+			if (left == ParityLattice.EVEN || right == ParityLattice.EVEN)
+				return ParityLattice.EVEN;
+
+			if (left == ParityLattice.ODD)
+				return right;
+		}
+
+		if (op instanceof ModuloOperator || op instanceof RemainderOperator) {
+			if (right == ParityLattice.EVEN)
+				return left;
+		}
+
+		return top();
+	}
+
+	// @Override
+	// public Satisfiability satisfiesBinaryExpression(
+	// BinaryExpression expression,
+	// ParityLattice left,
+	// ParityLattice right,
+	// ProgramPoint pp,
+	// SemanticOracle oracle) {
+
+	// if (left.isBottom() || right.isBottom())
+	// return Satisfiability.BOTTOM;
+
+	// BinaryOperator op = expression.getOperator();
+
+	// if (op instanceof ComparisonEq)
+	// return left.eq(right);
+
+	// if (op instanceof ComparisonNe)
+	// return left.eq(right).negate();
+
+	// return Satisfiability.UNKNOWN;
+
+	// }
+
+	// @Override
+	// public ValueEnvironment<ParityLattice> assumeBinaryExpression(
+	// ValueEnvironment<ParityLattice> environment,
+	// BinaryExpression expression,
+	// ProgramPoint src,
+	// ProgramPoint dest,
+	// SemanticOracle oracle)
+	// throws SemanticException {
+
+	// BinaryOperator op = expression.getOperator();
+
+	// if (!(op instanceof ComparisonEq || op instanceof ComparisonNe))
+	// return environment;
+
+	// ParityLattice evalued;
+
+	// ValueExpression left = ((ValueExpression) expression.getLeft());
+	// ValueExpression right = ((ValueExpression) expression.getRight());
+
+	// if (left instanceof Identifier identifier) {
+	// ParityLattice rval = eval(environment, right, src, oracle);
+
+	// if (rval == ParityLattice.EVEN || rval == ParityLattice.ODD) {
+
+	// if (op instanceof ComparisonEq)
+	// evalued = rval;
+	// else
+	// evalued = (rval == ParityLattice.EVEN) ? ParityLattice.ODD :
+	// ParityLattice.EVEN;
+
+	// environment = environment.putState(identifier,
+	// environment.getState(identifier).glb(evalued));
+
+	// }
+	// }
+
+	// if (right instanceof Identifier identifier) {
+	// ParityLattice lval = eval(environment, left, src, oracle);
+
+	// if (lval == ParityLattice.EVEN || lval == ParityLattice.ODD) {
+
+	// if (op instanceof ComparisonEq)
+	// evalued = lval;
+
+	// else
+	// evalued = (lval == ParityLattice.EVEN) ? ParityLattice.ODD :
+	// ParityLattice.EVEN;
+
+	// environment = environment.putState(identifier,
+	// environment.getState(identifier).glb(evalued));
+	// }
+	// }
+
+	// return environment;
+	// }
 }
--- src/main/java/it/unive/scsr/analysis/parity/ParityLattice.java
+++ src/main/java/it/unive/scsr/analysis/parity/ParityLattice.java
@@ -1,41 +1,95 @@
 package it.unive.scsr.analysis.parity;
 
+import java.util.Objects;
+
 import it.unive.lisa.analysis.BaseLattice;
+import it.unive.lisa.analysis.Lattice;
 import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.util.representation.StringRepresentation;
 import it.unive.lisa.util.representation.StructuredRepresentation;
 
 //Homework
 public class ParityLattice implements BaseLattice<ParityLattice> {
 
+	public static final ParityLattice EVEN = new ParityLattice(0);
+	public static final ParityLattice ODD = new ParityLattice(1);
+	public static final ParityLattice TOP = new ParityLattice(2);
+	public static final ParityLattice BOTTOM = new ParityLattice(-1);
+
+	private final Integer parity;
+
+	public ParityLattice(Integer p) {
+		this.parity = p;
+	}
+
 	@Override
 	public ParityLattice top() {
-		// TODO: homework
-		return null;
+		return ParityLattice.TOP;
 	}
 
 	@Override
 	public ParityLattice bottom() {
-		// TODO: homework
-		return null;
+		return ParityLattice.BOTTOM;
 	}
 
 	@Override
 	public StructuredRepresentation representation() {
-		// TODO: homework
-		return null;
+		if (this == ParityLattice.TOP)
+			return Lattice.topRepresentation();
+		else if (this == ParityLattice.BOTTOM)
+			return Lattice.bottomRepresentation();
+		else if (this == ParityLattice.EVEN)
+			return new StringRepresentation("EVEN");
+
+		return new StringRepresentation("ODD");
 	}
 
 	@Override
 	public ParityLattice lubAux(ParityLattice other) throws SemanticException {
-		// TODO: homework
-		return null;
+		return ParityLattice.TOP;
 	}
 
 	@Override
 	public boolean lessOrEqualAux(ParityLattice other) throws SemanticException {
-		// TODO: homework
 		return false;
 	}
 
-	// TODO add missing components
+	// public Satisfiability eq(ParityLattice other) {
+	// if (this.isBottom() || other.isBottom())
+	// return Satisfiability.BOTTOM;
+
+	// if (this.isTop() || other.isTop())
+	// return Satisfiability.UNKNOWN;
+
+	// if (this.equals(other) == false)
+	// return Satisfiability.NOT_SATISFIED;
+
+	// // If both have the same parity -> could be equal, but not guaranteed
+	// return Satisfiability.UNKNOWN;
+	// }
+
+	// public Satisfiability gt(ParityLattice other) {
+	// if (this.isBottom() || other.isBottom())
+	// return Satisfiability.BOTTOM;
+
+	// return Satisfiability.UNKNOWN;
+	// }
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.parity);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+
+		if (obj == null || !(obj instanceof ParityLattice))
+			return false;
+
+		ParityLattice other = (ParityLattice) obj;
+
+		return Objects.equals(this.parity, other.parity);
+	}
 }

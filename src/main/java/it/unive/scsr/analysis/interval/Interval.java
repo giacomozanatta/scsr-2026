1d013e0f90d3d41017af551a3d93e0c354bdf3f3
Marco <terrinmarco99@gmail.com>
2026-04-21 11:37:43 +0200

    Task 3

--- src/main/java/it/unive/scsr/analysis/interval/Interval.java
+++ src/main/java/it/unive/scsr/analysis/interval/Interval.java
@@ -1,5 +1,7 @@
 package it.unive.scsr.analysis.interval;
 
+import java.util.List;
+
 import it.unive.lisa.analysis.SemanticException;
 import it.unive.lisa.analysis.SemanticOracle;
 import it.unive.lisa.analysis.nonrelational.value.BaseNonRelationalValueDomain;
@@ -12,7 +14,9 @@ import it.unive.lisa.symbolic.value.operator.DivisionOperator;
 import it.unive.lisa.symbolic.value.operator.MultiplicationOperator;
 import it.unive.lisa.symbolic.value.operator.SubtractionOperator;
 import it.unive.lisa.symbolic.value.operator.unary.NumericNegation;
+import it.unive.lisa.util.numeric.IntInterval;
 import it.unive.lisa.util.numeric.MathNumber;
+import it.unive.lisa.symbolic.value.operator.binary.BinaryOperator;
 
 public class Interval implements BaseNonRelationalValueDomain<IntervalLattice>{
 
@@ -63,32 +67,59 @@ public class Interval implements BaseNonRelationalValueDomain<IntervalLattice>{
 	public IntervalLattice evalBinaryExpression(BinaryExpression expression, IntervalLattice left,
 			IntervalLattice right, ProgramPoint pp, SemanticOracle oracle) throws SemanticException {
 		
-		if(left.i == null || right == null)
+		if(left.i == null || right.i == null)
 			return IntervalLattice.BOTTOM;
 		
-		if(expression.getOperator() instanceof AdditionOperator) {
-			
-			MathNumber u1 = left.i.getHigh();
-			MathNumber u2 = right.i.getHigh();
-			
-			MathNumber l1 = left.i.getLow();
-			MathNumber l2 = right.i.getLow();
-			
-			return new IntervalLattice(l1.add(l2), u1.add(u2));
-			
-		} else if (expression.getOperator() instanceof MultiplicationOperator) {
-			// TODO: homework
-		} else if (expression.getOperator() instanceof SubtractionOperator) {
-			// TODO: homework
-		} else if (expression.getOperator() instanceof DivisionOperator) {
-			// TODO: homework
+		MathNumber upperLeft = left.i.getHigh();
+		MathNumber upperRight = right.i.getHigh();
+
+		MathNumber lowerLeft = left.i.getLow();
+		MathNumber lowerRight = right.i.getLow();
+
+		BinaryOperator op = expression.getOperator();
+
+		if (op instanceof AdditionOperator) {
+			return new IntervalLattice(lowerLeft.add(lowerRight), upperLeft.add(upperRight));
+
+		} else if (op instanceof SubtractionOperator) {
+			return new IntervalLattice(lowerLeft.subtract(upperRight),upperLeft.subtract(lowerRight));
+
+		} else if (op instanceof MultiplicationOperator) {
+			if (left.equals(IntervalLattice.ZERO) || right.equals(IntervalLattice.ZERO))
+				return IntervalLattice.ZERO;
+
+			MathNumber n1 = lowerLeft.multiply(lowerRight);
+			MathNumber n2 = lowerLeft.multiply(upperRight);
+			MathNumber n3 = upperLeft.multiply(lowerRight);
+			MathNumber n4 = upperLeft.multiply(upperRight);
+
+			MathNumber min = n1.min(n2).min(n3).min(n4);
+			MathNumber max = n1.max(n2).max(n3).max(n4);
+
+			return new IntervalLattice(min, max);
+
+		} else if (op instanceof DivisionOperator) {
+			if (right.i.includes(IntInterval.ZERO)) {
+    			if (right.equals(IntervalLattice.ZERO)) {
+        			return IntervalLattice.BOTTOM;
+    			} else {
+        			return IntervalLattice.TOP;
+    			}
+			}
+
+			if (left.equals(IntervalLattice.ZERO))
+				return IntervalLattice.ZERO;
+
+			MathNumber q1 = lowerLeft.divide(lowerRight);
+			MathNumber q2 = lowerLeft.divide(upperRight);
+			MathNumber q3 = upperLeft.divide(lowerRight);
+			MathNumber q4 = upperLeft.divide(upperRight);
+
+			MathNumber min = q1.min(q2).min(q3).min(q4);
+			MathNumber max = q1.max(q2).max(q3).max(q4);
+
+			return new IntervalLattice(min.roundDown(), max.roundUp());
 		}
-		
 		return IntervalLattice.TOP;
 	}
-
-	
-	
-	
-	
 }

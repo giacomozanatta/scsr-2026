271a620367d06a33aa36dcb0f8e8164d87888108
Larisa Dinulescu <larisadinulescu@gmail.com>
2026-04-21 18:02:14 +0200

    Task 4

--- src/main/java/it/unive/scsr/analysis/interval/Interval.java
+++ src/main/java/it/unive/scsr/analysis/interval/Interval.java
@@ -14,7 +14,7 @@ import it.unive.lisa.symbolic.value.operator.SubtractionOperator;
 import it.unive.lisa.symbolic.value.operator.unary.NumericNegation;
 import it.unive.lisa.util.numeric.MathNumber;
 
-public class Interval implements BaseNonRelationalValueDomain<IntervalLattice>{
+public class Interval implements BaseNonRelationalValueDomain<IntervalLattice> {
 
 	@Override
 	public IntervalLattice top() {
@@ -25,70 +25,75 @@ public class Interval implements BaseNonRelationalValueDomain<IntervalLattice>{
 	public IntervalLattice bottom() {
 		return IntervalLattice.BOTTOM;
 	}
-	
-	@Override
-	public IntervalLattice 
-	evalConstant(Constant constant, ProgramPoint pp, SemanticOracle oracle)
-			throws SemanticException {
-		
-		if(constant.getValue() instanceof Integer) {
-			//I need to check the integer value to 
-			// assign the right approx value
-			Integer n = (Integer) constant.getValue();
 
+	@Override
+	public IntervalLattice evalConstant(Constant constant, ProgramPoint pp, SemanticOracle oracle)
+			throws SemanticException {
+		if (constant.getValue() instanceof Integer) {
+			Integer n = (Integer) constant.getValue();
 			return new IntervalLattice(n, n);
 		}
-			
 		return IntervalLattice.TOP;
 	}
 
 	@Override
 	public IntervalLattice evalUnaryExpression(UnaryExpression expression, IntervalLattice arg, ProgramPoint pp,
-			SemanticOracle oracle) throws SemanticException {
-
-		if(arg.i == null)
+	                                           SemanticOracle oracle) throws SemanticException {
+		if (arg.isBottom())
 			return IntervalLattice.BOTTOM;
-		
-		if(expression.getOperator() == NumericNegation.INSTANCE) {
+
+		if (expression.getOperator() == NumericNegation.INSTANCE) {
 			MathNumber u = arg.i.getHigh();
 			MathNumber l = arg.i.getLow();
-			
-			return new IntervalLattice(u.multiply(MathNumber.MINUS_ONE),l.multiply(MathNumber.MINUS_ONE));
+			return new IntervalLattice(u.multiply(MathNumber.MINUS_ONE), l.multiply(MathNumber.MINUS_ONE));
 		}
-	
 		return IntervalLattice.TOP;
 	}
 
 	@Override
 	public IntervalLattice evalBinaryExpression(BinaryExpression expression, IntervalLattice left,
-			IntervalLattice right, ProgramPoint pp, SemanticOracle oracle) throws SemanticException {
-		
-		if(left.i == null || right == null)
+	                                            IntervalLattice right, ProgramPoint pp, SemanticOracle oracle) throws SemanticException {
+
+		if (left.isBottom() || right.isBottom())
 			return IntervalLattice.BOTTOM;
-		
-		if(expression.getOperator() instanceof AdditionOperator) {
-			
-			MathNumber u1 = left.i.getHigh();
-			MathNumber u2 = right.i.getHigh();
-			
-			MathNumber l1 = left.i.getLow();
-			MathNumber l2 = right.i.getLow();
-			
+
+		MathNumber l1 = left.i.getLow();
+		MathNumber u1 = left.i.getHigh();
+		MathNumber l2 = right.i.getLow();
+		MathNumber u2 = right.i.getHigh();
+
+		if (expression.getOperator() instanceof AdditionOperator) {
 			return new IntervalLattice(l1.add(l2), u1.add(u2));
-			
-		} else if (expression.getOperator() instanceof MultiplicationOperator) {
-			// TODO: homework
+
 		} else if (expression.getOperator() instanceof SubtractionOperator) {
-			// TODO: homework
+			return new IntervalLattice(l1.subtract(u2), u1.subtract(l2));
+
+		} else if (expression.getOperator() instanceof MultiplicationOperator) {
+			MathNumber v1 = safeMultiply(l1, l2);
+			MathNumber v2 = safeMultiply(l1, u2);
+			MathNumber v3 = safeMultiply(u1, l2);
+			MathNumber v4 = safeMultiply(u1, u2);
+
+			return new IntervalLattice(v1.min(v2).min(v3).min(v4), v1.max(v2).max(v3).max(v4));
+
 		} else if (expression.getOperator() instanceof DivisionOperator) {
-			// TODO: homework
+			if (l2.leq(MathNumber.ZERO) && u2.geq(MathNumber.ZERO))
+				return IntervalLattice.TOP;
+
+			MathNumber v1 = l1.divide(l2);
+			MathNumber v2 = l1.divide(u2);
+			MathNumber v3 = u1.divide(l2);
+			MathNumber v4 = u1.divide(u2);
+
+			return new IntervalLattice(v1.min(v2).min(v3).min(v4), v1.max(v2).max(v3).max(v4));
 		}
-		
+
 		return IntervalLattice.TOP;
 	}
 
-	
-	
-	
-	
+	private MathNumber safeMultiply(MathNumber a, MathNumber b) {
+		if ((a.isZero() && b.isInfinite()) || (a.isInfinite() && b.isZero()))
+			return MathNumber.ZERO;
+		return a.multiply(b);
+	}
 }
--- src/main/java/it/unive/scsr/analysis/interval/IntervalLattice.java
+++ src/main/java/it/unive/scsr/analysis/interval/IntervalLattice.java
@@ -1,7 +1,6 @@
 package it.unive.scsr.analysis.interval;
 
 import java.util.Objects;
-
 import it.unive.lisa.analysis.BaseLattice;
 import it.unive.lisa.analysis.Lattice;
 import it.unive.lisa.analysis.SemanticException;
@@ -10,173 +9,85 @@ import it.unive.lisa.util.numeric.MathNumber;
 import it.unive.lisa.util.representation.StringRepresentation;
 import it.unive.lisa.util.representation.StructuredRepresentation;
 
-public class IntervalLattice 
-		implements BaseLattice<IntervalLattice>, Comparable<IntervalLattice> {
-	
+public class IntervalLattice implements BaseLattice<IntervalLattice>, Comparable<IntervalLattice> {
+
 	IntInterval i;
-	
-	public static IntervalLattice TOP = new IntervalLattice(MathNumber.MINUS_INFINITY, MathNumber.PLUS_INFINITY);
-	public static IntervalLattice BOTTOM = new IntervalLattice(null);
-	public static IntervalLattice ZERO = new IntervalLattice(0,0);
-	
+
+	public static final IntervalLattice TOP = new IntervalLattice(MathNumber.MINUS_INFINITY, MathNumber.PLUS_INFINITY);
+	public static final IntervalLattice BOTTOM = new IntervalLattice(null);
+
 	public IntervalLattice(IntInterval i) {
 		this.i = i;
 	}
-	
+
 	public IntervalLattice(MathNumber l, MathNumber u) {
-		this.i = new IntInterval(l, u);
+		this.i = (l == null || u == null) ? null : new IntInterval(l, u);
 	}
-	
+
 	public IntervalLattice(int l, int u) {
 		this.i = new IntInterval(l, u);
 	}
-	
-	public IntervalLattice() {
-		this(IntInterval.INFINITY);
-	}
-	
 
 	@Override
-	    public IntervalLattice top() {
-			return TOP;
-	    }
+	public IntervalLattice top() { return TOP; }
 
-	    @Override
-	    public IntervalLattice bottom() {
-	    	return BOTTOM;
-	    }
+	@Override
+	public IntervalLattice bottom() { return BOTTOM; }
 
-	    @Override
-	    public StructuredRepresentation representation() {
-	    	if(this == BOTTOM)
-	    		return Lattice.bottomRepresentation();
-	    	
-	    	MathNumber l = this.i.getLow();
-	    	MathNumber u = this.i.getHigh();
-	    	
-	    	return new StringRepresentation("["+l+","+u+"]");
-	    }
+	@Override
+	public boolean isBottom() { return i == null; }
 
-	    @Override
-	    public IntervalLattice lubAux(IntervalLattice other) throws SemanticException {
-	
-	    	if(this.i == null || other.i == null)
-	    		return BOTTOM;
-	    	
-	    	MathNumber l1 = this.i.getLow();
-	    	MathNumber l2 = other.i.getLow();
-	    	
-	    	MathNumber lResult;
-	    	if(l1.leq(l2))
-	    		lResult =l1;
-	    	else 
-	    		lResult = l2;
-	    	
-	    	MathNumber u1 = this.i.getHigh();
-	    	MathNumber u2 = other.i.getHigh();
-	    	
-	    	MathNumber uResult;
-	    	
-	    	if(u1.geq(u2))
-	    		uResult = u1;
-	    	else
-	    		uResult = u2;
-	    	
-	    	return new IntervalLattice(lResult,uResult);
-	    }
-	    
-	    
+	@Override
+	public StructuredRepresentation representation() {
+		if (isBottom()) return Lattice.bottomRepresentation();
+		if (this.equals(TOP)) return Lattice.topRepresentation();
+		return new StringRepresentation("[" + i.getLow() + ", " + i.getHigh() + "]");
+	}
 
-	    @Override
-		public IntervalLattice glbAux(IntervalLattice other) throws SemanticException {
+	@Override
+	public IntervalLattice lubAux(IntervalLattice other) throws SemanticException {
+		MathNumber low = i.getLow().min(other.i.getLow());
+		MathNumber high = i.getHigh().max(other.i.getHigh());
+		return new IntervalLattice(low, high);
+	}
 
-	    	
-	    	if(this.i == null || other.i == null)
-	    		return BOTTOM;
-	    	
-	    	MathNumber l1 = this.i.getLow();
-	    	MathNumber l2 = other.i.getLow();
-	    	
-	    	MathNumber lResult;
-	    	if(l1.geq(l2))
-	    		lResult = l1;
-	    	else 
-	    		lResult = l2;
-	    	
-	    	MathNumber u1 = this.i.getHigh();
-	    	MathNumber u2 = other.i.getHigh();
-	    	
-	    	MathNumber uResult;
-	    	if(u1.leq(u2))
-	    		uResult = u1;
-	    	else
-	    		uResult = u2;
+	@Override
+	public IntervalLattice glbAux(IntervalLattice other) throws SemanticException {
+		MathNumber low = i.getLow().max(other.i.getLow());
+		MathNumber high = i.getHigh().min(other.i.getHigh());
+		if (low.gt(high)) return BOTTOM;
+		return new IntervalLattice(low, high);
+	}
 
-			return new IntervalLattice(lResult, uResult);
-		}
+	@Override
+	public boolean lessOrEqualAux(IntervalLattice other) throws SemanticException {
+		return other.i.includes(this.i);
+	}
 
-		@Override
-	    public boolean lessOrEqualAux(IntervalLattice other) throws SemanticException {
-			if(this.i == null || other.i == null)
-				return false;
-	    	return this.i.includes(other.i);
-	    }
+	@Override
+	public IntervalLattice wideningAux(IntervalLattice other) throws SemanticException {
+		MathNumber low = other.i.getLow().lt(i.getLow()) ? MathNumber.MINUS_INFINITY : i.getLow();
+		MathNumber high = other.i.getHigh().gt(i.getHigh()) ? MathNumber.PLUS_INFINITY : i.getHigh();
+		return new IntervalLattice(low, high);
+	}
 
-		@Override
-		public IntervalLattice wideningAux(IntervalLattice other) throws SemanticException {
-			
-	    	if(this.i == null || other.i == null)
-	    		return BOTTOM;
-	    	
-			MathNumber u1 = this.i.getHigh();
-			MathNumber u2 = other.i.getHigh();
-			
-			MathNumber uResult = u1;
-			if(u2.geq(u1))
-				uResult = MathNumber.PLUS_INFINITY;
-			
-			MathNumber l1 = this.i.getLow();
-			MathNumber l2 = other.i.getLow();
-			
-			MathNumber lResult = l1;
-			if(l2.leq(l1)) {
-				lResult = MathNumber.MINUS_INFINITY;
-			}
-			
-			return new IntervalLattice(lResult, uResult);
-			
-		}
+	@Override
+	public int hashCode() { return Objects.hash(i); }
 
-		@Override
-		public int hashCode() {
-			return Objects.hash(i);
-		}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null || getClass() != obj.getClass()) return false;
+		IntervalLattice other = (IntervalLattice) obj;
+		return Objects.equals(i, other.i);
+	}
 
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			IntervalLattice other = (IntervalLattice) obj;
-			return Objects.equals(i, other.i);
-		}
-
-		@Override
-		public int compareTo(IntervalLattice o) {
-			if(isBottom())
-				return o.isBottom() ? 0 : -1; 
-			if(isTop())
-				return o.isTop() ? 0 : 1;
-			
-			if(o.isBottom())
-				return 1;
-			
-			if(isTop())
-				return -1;
-			
-			return i.compareTo(o.i);
-		}
+	@Override
+	public int compareTo(IntervalLattice o) {
+		if (this.isBottom()) return o.isBottom() ? 0 : -1;
+		if (o.isBottom()) return 1;
+		return this.i.getLow().equals(o.i.getLow())
+				? this.i.getHigh().compareTo(o.i.getHigh())
+				: this.i.getLow().compareTo(o.i.getLow());
+	}
 }

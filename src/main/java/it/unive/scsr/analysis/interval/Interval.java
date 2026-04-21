d7aa3bdfcb7f70f70fb081e3c75ac95cce6ab7d3
Prince <princedonkor101@gmail.com>
2026-04-21 13:25:47 +0200

    Task 4 - completed

--- src/main/java/it/unive/scsr/analysis/interval/Interval.java
+++ src/main/java/it/unive/scsr/analysis/interval/Interval.java
@@ -63,25 +63,70 @@ public class Interval implements BaseNonRelationalValueDomain<IntervalLattice>{
 	public IntervalLattice evalBinaryExpression(BinaryExpression expression, IntervalLattice left,
 			IntervalLattice right, ProgramPoint pp, SemanticOracle oracle) throws SemanticException {
 		
-		if(left.i == null || right == null)
+		if(left.i == null || right.i == null)
 			return IntervalLattice.BOTTOM;
+
+		MathNumber u1 = left.i.getHigh();
+		MathNumber u2 = right.i.getHigh();
+		MathNumber l1 = left.i.getLow();
+		MathNumber l2 = right.i.getLow();
 		
 		if(expression.getOperator() instanceof AdditionOperator) {
-			
-			MathNumber u1 = left.i.getHigh();
-			MathNumber u2 = right.i.getHigh();
-			
-			MathNumber l1 = left.i.getLow();
-			MathNumber l2 = right.i.getLow();
-			
 			return new IntervalLattice(l1.add(l2), u1.add(u2));
 			
 		} else if (expression.getOperator() instanceof MultiplicationOperator) {
-			// TODO: homework
+			MathNumber p1 = l1.multiply(l2);
+			MathNumber p2 = l1.multiply(u2);
+			MathNumber p3 = u1.multiply(l2);
+			MathNumber p4 = u1.multiply(u2);
+
+			MathNumber min = p1;
+			if (p2.leq(min))
+				min = p2;
+			if (p3.leq(min))
+				min = p3;
+			if (p4.leq(min))
+				min = p4;
+
+			MathNumber max = p1;
+			if (p2.geq(max))
+				max = p2;
+			if (p3.geq(max))
+				max = p3;
+			if (p4.geq(max))
+				max = p4;
+
+			return new IntervalLattice(min, max);
 		} else if (expression.getOperator() instanceof SubtractionOperator) {
-			// TODO: homework
+			return new IntervalLattice(l1.subtract(u2), u1.subtract(l2));
 		} else if (expression.getOperator() instanceof DivisionOperator) {
-			// TODO: homework
+			MathNumber zero = MathNumber.ZERO;
+			boolean divisorMayBeZero = l2.leq(zero) && u2.geq(zero);
+			if (divisorMayBeZero)
+				return IntervalLattice.TOP;
+
+			MathNumber d1 = l1.divide(l2);
+			MathNumber d2 = l1.divide(u2);
+			MathNumber d3 = u1.divide(l2);
+			MathNumber d4 = u1.divide(u2);
+
+			MathNumber min = d1;
+			if (d2.leq(min))
+				min = d2;
+			if (d3.leq(min))
+				min = d3;
+			if (d4.leq(min))
+				min = d4;
+
+			MathNumber max = d1;
+			if (d2.geq(max))
+				max = d2;
+			if (d3.geq(max))
+				max = d3;
+			if (d4.geq(max))
+				max = d4;
+
+			return new IntervalLattice(min, max);
 		}
 		
 		return IntervalLattice.TOP;

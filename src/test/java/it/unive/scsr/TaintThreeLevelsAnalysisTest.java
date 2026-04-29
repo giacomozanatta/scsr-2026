bf73f851bc082b3cb6d420cefd50ec6f6d5b65b5
890939NicolaMaso <890939@stud.unive.it>
2026-04-29 11:19:52 +0200

    Fix TaintThreeLevelsAnalysisTest

--- src/test/java/it/unive/scsr/TaintThreeLevelsAnalysisTest.java
+++ src/test/java/it/unive/scsr/TaintThreeLevelsAnalysisTest.java
@@ -3,10 +3,12 @@ package it.unive.scsr;
 import it.unive.lisa.AnalysisException;
 import it.unive.lisa.DefaultConfiguration;
 import it.unive.lisa.LiSA;
+import it.unive.lisa.analysis.heap.pointbased.PointBasedHeap;
 import it.unive.lisa.analysis.informationFlow.BaseTaint;
 import it.unive.lisa.conf.LiSAConfiguration;
 import it.unive.lisa.imp.IMPFrontend;
 import it.unive.lisa.imp.ParsingException;
+import it.unive.lisa.interprocedural.context.ContextBasedAnalysis;
 import it.unive.lisa.outputs.HtmlResults;
 import it.unive.lisa.outputs.JSONReportDumper;
 import it.unive.lisa.program.Program;
@@ -40,7 +42,8 @@ public class TaintThreeLevelsAnalysisTest {
         //conf.outputs.add(new HtmlInputs(true));
         conf.outputs.add(new HtmlResults<>(true));
         // we specify the analysis that we want to execute
-        conf.analysis = simpleDomain(defaultHeapDomain(), new TaintThreeLevels(), defaultTypeDomain());
+        conf.analysis = simpleDomain(new PointBasedHeap(), new TaintThreeLevels(), defaultTypeDomain());
+        conf.interproceduralAnalysis = new ContextBasedAnalysis<>();
 
         for(CFG cfg : program.getAllCFGs()) {
             String name = cfg.getDescriptor().getName();

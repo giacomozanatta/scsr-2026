ce312c97b00de1ab13beeb9416320dc2d7f40d0a
paolotito1803 <titotto.paolo@gmail.com>
2026-04-29 12:01:39 +0200

    Update TaintAnalysisTest.java

--- src/test/java/it/unive/scsr/TaintAnalysisTest.java
+++ src/test/java/it/unive/scsr/TaintAnalysisTest.java
@@ -3,14 +3,14 @@ package it.unive.scsr;
 import it.unive.lisa.AnalysisException;
 import it.unive.lisa.DefaultConfiguration;
 import it.unive.lisa.LiSA;
+import it.unive.lisa.analysis.heap.pointbased.PointBasedHeap;
 import it.unive.lisa.analysis.informationFlow.BaseTaint;
-import it.unive.scsr.analysis.sign.NonNegativeSpeedInMoveForwardChecker;
-import it.unive.scsr.analysis.sign.Sign;
 import it.unive.scsr.analysis.taint.Taint;
 import it.unive.scsr.analysis.taint.TaintChecker;
 import it.unive.lisa.conf.LiSAConfiguration;
 import it.unive.lisa.imp.IMPFrontend;
 import it.unive.lisa.imp.ParsingException;
+import it.unive.lisa.interprocedural.context.ContextBasedAnalysis;
 import it.unive.lisa.outputs.HtmlResults;
 import it.unive.lisa.outputs.JSONReportDumper;
 import it.unive.lisa.program.Program;
@@ -42,8 +42,8 @@ public class TaintAnalysisTest {
         //conf.outputs.add(new HtmlInputs(true));
         conf.outputs.add(new HtmlResults<>(true));
         // we specify the analysis that we want to execute
-        conf.analysis = simpleDomain(defaultHeapDomain(), new Taint(), defaultTypeDomain());
-
+        conf.analysis = simpleDomain(new PointBasedHeap(), new Taint(), defaultTypeDomain());
+        conf.interproceduralAnalysis = new ContextBasedAnalysis<>();
         for(CFG cfg : program.getAllCFGs()) {
             String name = cfg.getDescriptor().getName();
             if(isSource(name))

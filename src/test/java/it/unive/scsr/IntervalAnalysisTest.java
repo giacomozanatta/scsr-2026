b354609ebff2a34cd4f2a172eb8b681c9250db35
Giacomo Bordignon <jack.bordignon003@gmail.com>
2026-05-04 14:40:20 +0200

    Added Interval Solution and checkers

--- src/test/java/it/unive/scsr/IntervalAnalysisTest.java
+++ src/test/java/it/unive/scsr/IntervalAnalysisTest.java
@@ -3,16 +3,32 @@ package it.unive.scsr;
 import it.unive.lisa.AnalysisException;
 import it.unive.lisa.DefaultConfiguration;
 import it.unive.lisa.LiSA;
+import it.unive.scsr.analysis.interval.Interval;
 import it.unive.lisa.conf.LiSAConfiguration;
 import it.unive.lisa.imp.IMPFrontend;
 import it.unive.lisa.imp.ParsingException;
 import it.unive.lisa.interprocedural.context.ContextBasedAnalysis;
 import it.unive.lisa.outputs.HtmlResults;
+import it.unive.lisa.outputs.JSONReportDumper;
+import it.unive.lisa.outputs.JSONResults;
+import it.unive.lisa.outputs.compare.ResultComparer;
+import it.unive.lisa.outputs.json.JsonReport;
 import it.unive.lisa.program.Program;
-import it.unive.scsr.analysis.interval.Interval;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import it.unive.scsr.analysis.interval.IntervalSolution;
 import org.junit.Test;
 
 import static it.unive.lisa.DefaultConfiguration.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class IntervalAnalysisTest {
 
@@ -28,16 +44,38 @@ public class IntervalAnalysisTest {
         conf.workdir = "outputs/intervals";
 
         // we specify the visual format of the analysis results
+        //conf.outputs.add(new HtmlInputs(true));
         conf.outputs.add(new HtmlResults<>(true));
+        conf.outputs.add(new HtmlResults<>(true));
+        conf.outputs.add(new JSONResults<>());
+        conf.outputs.add(new JSONReportDumper());
         // we specify the analysis that we want to execute
         conf.analysis = simpleDomain(defaultHeapDomain(), new Interval(), defaultTypeDomain());
-
         conf.interproceduralAnalysis = new ContextBasedAnalysis<>();
+        // added checker to the analysis
 
         // we instantiate LiSA with our configuration
         LiSA lisa = new LiSA(conf);
 
         // finally, we tell LiSA to analyze the program
         lisa.run(program);
+
+        Path expectedPath = Paths.get("expected", "intervals-eval");
+        Path actualPath = Paths.get("outputs", "intervals");
+
+        File expFile = Paths.get(expectedPath.toString(), "report.json").toFile();
+        File actFile = Paths.get(actualPath.toString(), "report.json").toFile();
+        try {
+            JsonReport expected = JsonReport.read(new FileReader(expFile));
+            JsonReport actual = JsonReport.read(new FileReader(actFile));
+            assertTrue("Results are different",
+                    new ResultComparer().compare(expected, actual, expectedPath.toFile(), actualPath.toFile()));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace(System.err);
+            fail("Unable to find report file");
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
+            fail("Unable to compare reports");
+        }
     }
 }

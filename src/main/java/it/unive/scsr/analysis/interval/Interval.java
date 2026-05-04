df69ba663db119ea8bd51904bc49a28118e20128
Luca Olivieri <oli.luca93@gmail.com>
2026-05-04 13:22:14 +0200

    minor fix

--- src/main/java/it/unive/scsr/analysis/interval/Interval.java
+++ src/main/java/it/unive/scsr/analysis/interval/Interval.java
@@ -102,7 +102,7 @@ public class Interval implements BaseNonRelationalValueDomain<IntervalLattice>{
 	public IntervalLattice evalBinaryExpression(BinaryExpression expression, IntervalLattice left,
 			IntervalLattice right, ProgramPoint pp, SemanticOracle oracle) throws SemanticException {
 		
-		if(left.i == null || right == null)
+		if(left.i == null || right.i == null)
 			return IntervalLattice.BOTTOM;
 		if(left.isBottom() || right.isBottom())
 			return IntervalLattice.BOTTOM;

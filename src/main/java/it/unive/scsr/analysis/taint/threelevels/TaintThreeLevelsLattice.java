2f0e3955accf714fd179383ae42d017294134f42
Giacomo Bordignon <jack.bordignon003@gmail.com>
2026-05-06 11:29:51 +0200

    Update fourth assignment submission
    This assignment is done with 895227-titotto-paolo

--- src/main/java/it/unive/scsr/analysis/taint/threelevels/TaintThreeLevelsLattice.java
+++ src/main/java/it/unive/scsr/analysis/taint/threelevels/TaintThreeLevelsLattice.java
@@ -90,10 +90,10 @@ public class TaintThreeLevelsLattice implements it.unive.lisa.lattices.informati
     public TaintThreeLevelsLattice or(TaintThreeLevelsLattice other) throws SemanticException {
         if(this.equals(BOTTOM) || other.equals(BOTTOM))
             return BOTTOM;
-        else if(this.equals(TOP) || other.equals(TOP))
-            return TOP;
         else if(this.equals(Taint) || other.equals(Taint))
             return Taint;
+        else if(this.equals(TOP) || other.equals(TOP))
+            return TOP;
         return Clean;
     }
 

ad3c922e713a483a3ed19c6e6d8e59ba70b64daa
Daniele Dotto <daniele.dotto@electrolux.com>
2026-05-06 18:08:52 +0200

    feat: implemented task 4

--- src/main/java/it/unive/scsr/analysis/interval/Interval.java
+++ src/main/java/it/unive/scsr/analysis/interval/Interval.java
@@ -76,12 +76,62 @@ public class Interval implements BaseNonRelationalValueDomain<IntervalLattice>{
 			
 			return new IntervalLattice(l1.add(l2), u1.add(u2));
 			
-		} else if (expression.getOperator() instanceof MultiplicationOperator) {
-			// TODO: homework
 		} else if (expression.getOperator() instanceof SubtractionOperator) {
-			// TODO: homework
+			// [l1, u1] - [l2, u2] = [l1 - u2, u1 - l2]
+			MathNumber l1 = left.i.getLow();
+			MathNumber u1 = left.i.getHigh();
+			MathNumber l2 = right.i.getLow();
+			MathNumber u2 = right.i.getHigh();
+			return new IntervalLattice(l1.subtract(u2), u1.subtract(l2));
+
+		} else if (expression.getOperator() instanceof MultiplicationOperator) {
+			MathNumber l1 = left.i.getLow();
+			MathNumber u1 = left.i.getHigh();
+			MathNumber l2 = right.i.getLow();
+			MathNumber u2 = right.i.getHigh();
+
+			// Compute all four products and take [min, max]
+			MathNumber a = l1.multiply(l2);
+			MathNumber b = l1.multiply(u2);
+			MathNumber c = u1.multiply(l2);
+			MathNumber d = u1.multiply(u2);
+
+			return new IntervalLattice(
+					a.min(b).min(c).min(d),
+					a.max(b).max(c).max(d));
+
 		} else if (expression.getOperator() instanceof DivisionOperator) {
-			// TODO: homework
+			MathNumber l1 = left.i.getLow();
+			MathNumber u1 = left.i.getHigh();
+			MathNumber l2 = right.i.getLow();
+			MathNumber u2 = right.i.getHigh();
+
+			// 0 / x = 0 for any x
+			if (l1.isZero() && u1.isZero())
+				return IntervalLattice.ZERO;
+
+			// x / [0,0] is undefined -> TOP (sound over-approximation)
+			if (l2.isZero() && u2.isZero())
+				return IntervalLattice.TOP;
+
+			// If 0 is strictly outside [l2, u2], we can safely compute the reciprocal
+			if (l2.compareTo(MathNumber.ZERO) > 0 || u2.compareTo(MathNumber.ZERO) < 0) {
+				// [l1,u1] / [l2,u2] = [l1,u1] * [1/u2, 1/l2]
+				MathNumber invLow = MathNumber.ONE.divide(u2);
+				MathNumber invHigh = MathNumber.ONE.divide(l2);
+
+				MathNumber a = l1.multiply(invLow);
+				MathNumber b = l1.multiply(invHigh);
+				MathNumber c = u1.multiply(invLow);
+				MathNumber d = u1.multiply(invHigh);
+
+				return new IntervalLattice(
+						a.min(b).min(c).min(d).roundDown(),
+						a.max(b).max(c).max(d).roundUp());
+			}
+
+			// 0 is inside the denominator interval -> conservative approximation
+			return IntervalLattice.TOP;
 		}
 		
 		return IntervalLattice.TOP;

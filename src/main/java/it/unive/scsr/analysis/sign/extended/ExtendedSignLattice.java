581ba2bd5c556d93d423dd29942f6fcd6d49d7f4
sebastianoboato <sebastianoboato@gmail.com>
2026-05-10 17:24:28 +0200

    Add ExtendedSing class

--- src/main/java/it/unive/scsr/analysis/sign/extended/ExtendedSignLattice.java
+++ src/main/java/it/unive/scsr/analysis/sign/extended/ExtendedSignLattice.java
@@ -14,14 +14,14 @@ public class ExtendedSignLattice implements BaseLattice<ExtendedSignLattice> {
     private final boolean includesZero;
     private final boolean includesPos;
 
-    public static final ExtendedSignLattice BOTTOM  = new ExtendedSignLattice(false, false, false);
-    public static final ExtendedSignLattice LT_ZERO = new ExtendedSignLattice(true,  false, false);
-    public static final ExtendedSignLattice EQ_ZERO = new ExtendedSignLattice(false, true,  false);
+    public static final ExtendedSignLattice BOTTOM = new ExtendedSignLattice(false, false, false);
+    public static final ExtendedSignLattice LT_ZERO = new ExtendedSignLattice(true, false, false);
+    public static final ExtendedSignLattice EQ_ZERO = new ExtendedSignLattice(false, true, false);
     public static final ExtendedSignLattice GT_ZERO = new ExtendedSignLattice(false, false, true);
-    public static final ExtendedSignLattice LE_ZERO = new ExtendedSignLattice(true,  true,  false);
-    public static final ExtendedSignLattice NE_ZERO = new ExtendedSignLattice(true,  false, true);
-    public static final ExtendedSignLattice GE_ZERO = new ExtendedSignLattice(false, true,  true);
-    public static final ExtendedSignLattice TOP     = new ExtendedSignLattice(true,  true,  true);
+    public static final ExtendedSignLattice LE_ZERO = new ExtendedSignLattice(true, true, false);
+    public static final ExtendedSignLattice NE_ZERO = new ExtendedSignLattice(true, false, true);
+    public static final ExtendedSignLattice GE_ZERO = new ExtendedSignLattice(false, true, true);
+    public static final ExtendedSignLattice TOP = new ExtendedSignLattice(true, true, true);
 
     private ExtendedSignLattice(boolean includesNeg, boolean includesZero, boolean includesPos) {
         this.includesNeg = includesNeg;
@@ -39,38 +39,94 @@ public class ExtendedSignLattice implements BaseLattice<ExtendedSignLattice> {
         return BOTTOM;
     }
 
+    ExtendedSignLattice negate() {
+        return fromFlags(includesPos, includesZero, includesNeg);
+    }
+
+    ExtendedSignLattice add(ExtendedSignLattice other) {
+        boolean resNeg = this.includesNeg || other.includesNeg;
+        boolean resPos = this.includesPos || other.includesPos;
+        boolean resZero = (this.includesZero && other.includesZero)
+                || (this.includesPos && other.includesNeg)
+                || (this.includesNeg && other.includesPos);
+
+        return fromFlags(resNeg, resZero, resPos);
+    }
+
+    ExtendedSignLattice mul(ExtendedSignLattice other) {
+        boolean resNeg = (this.includesNeg && other.includesPos)
+                || (this.includesPos && other.includesNeg);
+        boolean resZero = this.includesZero || other.includesZero;
+        boolean resPos = (this.includesPos && other.includesPos)
+                || (this.includesNeg && other.includesNeg);
+
+        return fromFlags(resNeg, resZero, resPos);
+    }
+
+    ExtendedSignLattice div(ExtendedSignLattice other) {
+        if (!other.includesPos && !other.includesNeg) return BOTTOM;
+
+        boolean resNeg = (this.includesNeg && other.includesPos)
+                || (this.includesPos && other.includesNeg);
+        boolean resZero = this.includesZero && (other.includesPos || other.includesNeg);
+        boolean resPos = (this.includesPos && other.includesPos)
+                || (this.includesNeg && other.includesNeg);
+
+        return fromFlags(resNeg, resZero, resPos);
+    }
+
+    ExtendedSignLattice mod(ExtendedSignLattice other) {
+        if (!other.includesPos && !other.includesNeg) return BOTTOM;
+
+        boolean resNeg = other.includesNeg;
+        boolean resZero = true;
+        boolean resPos = other.includesPos;
+
+        return fromFlags(resNeg, resZero, resPos);
+    }
+
+    ExtendedSignLattice rem(ExtendedSignLattice other) {
+        if (!other.includesPos && !other.includesNeg) return BOTTOM;
+
+        boolean resNeg = this.includesNeg;
+        boolean resZero = true;
+        boolean resPos = this.includesPos;
+
+        return fromFlags(resNeg, resZero, resPos);
+    }
+
     private static ExtendedSignLattice fromFlags(boolean neg, boolean zero, boolean pos) {
         if (!neg && !zero && !pos) return BOTTOM;
-        if ( neg && !zero && !pos) return LT_ZERO;
-        if (!neg &&  zero && !pos) return EQ_ZERO;
-        if (!neg && !zero &&  pos) return GT_ZERO;
-        if ( neg &&  zero && !pos) return LE_ZERO;
-        if ( neg && !zero &&  pos) return NE_ZERO;
-        if (!neg &&  zero &&  pos) return GE_ZERO;
+        if (neg && !zero && !pos) return LT_ZERO;
+        if (!neg && zero && !pos) return EQ_ZERO;
+        if (!neg && !zero && pos) return GT_ZERO;
+        if (neg && zero && !pos) return LE_ZERO;
+        if (neg && !zero && pos) return NE_ZERO;
+        if (!neg && zero && pos) return GE_ZERO;
         return TOP;
     }
 
     @Override
     public ExtendedSignLattice lubAux(ExtendedSignLattice other) throws SemanticException {
         return fromFlags(
-            this.includesNeg  || other.includesNeg,
-            this.includesZero || other.includesZero,
-            this.includesPos  || other.includesPos
+                this.includesNeg || other.includesNeg,
+                this.includesZero || other.includesZero,
+                this.includesPos || other.includesPos
         );
     }
 
     @Override
     public boolean lessOrEqualAux(ExtendedSignLattice other) throws SemanticException {
-        if (this.includesNeg  && !other.includesNeg)  return false;
+        if (this.includesNeg && !other.includesNeg) return false;
         if (this.includesZero && !other.includesZero) return false;
-        if (this.includesPos  && !other.includesPos)  return false;
+        if (this.includesPos && !other.includesPos) return false;
         return true;
     }
 
     @Override
     public StructuredRepresentation representation() {
-        if (this == BOTTOM)  return Lattice.bottomRepresentation();
-        if (this == TOP)     return Lattice.topRepresentation();
+        if (this == BOTTOM) return Lattice.bottomRepresentation();
+        if (this == TOP) return Lattice.topRepresentation();
         if (this == LT_ZERO) return new StringRepresentation("<0");
         if (this == EQ_ZERO) return new StringRepresentation("=0");
         if (this == GT_ZERO) return new StringRepresentation(">0");
@@ -91,8 +147,8 @@ public class ExtendedSignLattice implements BaseLattice<ExtendedSignLattice> {
         if (getClass() != obj.getClass()) return false;
         ExtendedSignLattice other = (ExtendedSignLattice) obj;
         return this.includesNeg == other.includesNeg
-            && this.includesZero == other.includesZero
-            && this.includesPos == other.includesPos;
+                && this.includesZero == other.includesZero
+                && this.includesPos == other.includesPos;
     }
 
 }

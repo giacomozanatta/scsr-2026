326d8a0127854304bb735146cd929c07c6ed7429
Luca Olivieri <oli.luca93@gmail.com>
2026-05-19 15:30:34 +0200

    Added three taint solution

--- src/test/java/it/unive/scsr/TaintAnalysisTest.java
+++ src/test/java/it/unive/scsr/TaintAnalysisTest.java
@@ -7,6 +7,8 @@ import it.unive.lisa.analysis.heap.pointbased.PointBasedHeap;
 import it.unive.lisa.analysis.informationFlow.BaseTaint;
 import it.unive.scsr.analysis.taint.Taint;
 import it.unive.scsr.analysis.taint.TaintChecker;
+import it.unive.scsr.analysis.taint.threelevels.TaintThreeLevelsChecker;
+import it.unive.scsr.analysis.taint.threelevels.TaintThreeLevels;
 import it.unive.lisa.conf.LiSAConfiguration;
 import it.unive.lisa.imp.IMPFrontend;
 import it.unive.lisa.imp.ParsingException;
@@ -92,5 +94,44 @@ public class TaintAnalysisTest {
 			if(sink.equals(name))
 				return true;
 		return false;
+	}
+	
+	@Test
+	public void testThreeTaintAnalysis() throws ParsingException, AnalysisException {
+		// we parse the program to get the CFG representation of the code in it
+		Program program = IMPFrontend.processFile("inputs/taint.imp");
+
+		// we build a new configuration for the analysis
+		LiSAConfiguration conf = new DefaultConfiguration();
+
+		// we specify where we want files to be generated
+		conf.workdir = "outputs/taint";
+
+		// we specify the visual format of the analysis results
+		//conf.outputs.add(new HtmlInputs(true));
+		conf.outputs.add(new HtmlResults<>(true));
+		// we specify the analysis that we want to execute
+		conf.analysis = simpleDomain(new PointBasedHeap(), new TaintThreeLevels(), defaultTypeDomain());
+		conf.interproceduralAnalysis = new ContextBasedAnalysis<>();
+		for(CFG cfg : program.getAllCFGs()) {
+		String name = cfg.getDescriptor().getName();
+		if(isSource(name))
+			cfg.getDescriptor().addAnnotation(BaseTaint.TAINTED_ANNOTATION);
+		else if(isSanitizer(name))
+			cfg.getDescriptor().addAnnotation(BaseTaint.CLEAN_ANNOTATION);
+		else if(isSink(name))
+			cfg.getDescriptor().addAnnotation(TaintThreeLevels.SINK_ANNOTATION);
+		}
+	
+		// added checker to the analysis
+		conf.semanticChecks.add(new TaintThreeLevelsChecker<>());
+		// A report file (.json) containing the warning triggered by the analysis can be found in the analysis output folder 
+		conf.outputs.add(new JSONReportDumper());
+	
+		// we instantiate LiSA with our configuration
+		LiSA lisa = new LiSA(conf);
+	
+		// finally, we tell LiSA to analyze the program
+		lisa.run(program);
 	}
 }

278f3e7e39f4d030159829c328020c05fbc60cce
Prince <princedonkor101@gmail.com>
2026-05-25 00:17:57 +0200

    Add three-level taint benchmark programs and JUnit integration tests.

--- src/test/java/it/unive/scsr/TaintAnalysisTest.java
+++ src/test/java/it/unive/scsr/TaintAnalysisTest.java
@@ -3,14 +3,16 @@ package it.unive.scsr;
 import it.unive.lisa.AnalysisException;
 import it.unive.lisa.DefaultConfiguration;
 import it.unive.lisa.LiSA;
+import it.unive.lisa.analysis.heap.pointbased.PointBasedHeap;
 import it.unive.lisa.analysis.informationFlow.BaseTaint;
-import it.unive.scsr.analysis.sign.NonNegativeSpeedInMoveForwardChecker;
-import it.unive.scsr.analysis.sign.Sign;
 import it.unive.scsr.analysis.taint.Taint;
 import it.unive.scsr.analysis.taint.TaintChecker;
+import it.unive.scsr.analysis.taint.threelevels.TaintThreeLevels;
+import it.unive.scsr.analysis.taint.threelevels.TaintThreeLevelsChecker;
 import it.unive.lisa.conf.LiSAConfiguration;
 import it.unive.lisa.imp.IMPFrontend;
 import it.unive.lisa.imp.ParsingException;
+import it.unive.lisa.interprocedural.context.ContextBasedAnalysis;
 import it.unive.lisa.outputs.HtmlResults;
 import it.unive.lisa.outputs.JSONReportDumper;
 import it.unive.lisa.program.Program;
@@ -42,8 +44,8 @@ public class TaintAnalysisTest {
         //conf.outputs.add(new HtmlInputs(true));
         conf.outputs.add(new HtmlResults<>(true));
         // we specify the analysis that we want to execute
-        conf.analysis = simpleDomain(defaultHeapDomain(), new Taint(), defaultTypeDomain());
-
+        conf.analysis = simpleDomain(new PointBasedHeap(), new Taint(), defaultTypeDomain());
+        conf.interproceduralAnalysis = new ContextBasedAnalysis<>();
         for(CFG cfg : program.getAllCFGs()) {
         	String name = cfg.getDescriptor().getName();
         	if(isSource(name))
@@ -66,6 +68,31 @@ public class TaintAnalysisTest {
         // finally, we tell LiSA to analyze the program
         lisa.run(program);
     }
+
+	@Test
+	public void testTaintThreeLevelsAnalysis() throws ParsingException, AnalysisException {
+		Program program = IMPFrontend.processFile("inputs/taint.imp");
+
+		LiSAConfiguration conf = new DefaultConfiguration();
+		conf.workdir = "outputs/taint-three-levels";
+		conf.analysis = simpleDomain(new PointBasedHeap(), new TaintThreeLevels(), defaultTypeDomain());
+		conf.interproceduralAnalysis = new ContextBasedAnalysis<>();
+		for (CFG cfg : program.getAllCFGs()) {
+			String name = cfg.getDescriptor().getName();
+			if (isSource(name))
+				cfg.getDescriptor().addAnnotation(BaseTaint.TAINTED_ANNOTATION);
+			else if (isSanitizer(name))
+				cfg.getDescriptor().addAnnotation(BaseTaint.CLEAN_ANNOTATION);
+			else if (isSink(name))
+				cfg.getDescriptor().addAnnotation(TaintThreeLevels.SINK_ANNOTATION);
+		}
+
+		conf.semanticChecks.add(new TaintThreeLevelsChecker<>());
+		conf.outputs.add(new JSONReportDumper());
+
+		LiSA lisa = new LiSA(conf);
+		lisa.run(program);
+	}
 
 	private boolean isSource(String name) {
 		for(String src : nameSource)

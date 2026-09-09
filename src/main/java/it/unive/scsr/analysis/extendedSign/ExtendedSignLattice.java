85eb5b5aaa45bb82909c140d7986eca14e3fa341
Elia Stevanato <eliastevanato2003@gmail.com>
2026-09-09 10:15:14 +0200

    ExtendedSignLattice finished

--- src/main/java/it/unive/scsr/analysis/extendedSign/ExtendedSignLattice.java
+++ src/main/java/it/unive/scsr/analysis/extendedSign/ExtendedSignLattice.java
@@ -3,6 +3,7 @@ package it.unive.scsr.analysis.extendedSign;
 import it.unive.lisa.analysis.BaseLattice;
 import it.unive.lisa.analysis.Lattice;
 import it.unive.lisa.analysis.SemanticException;
+import it.unive.lisa.lattices.Satisfiability;
 import it.unive.lisa.util.representation.StringRepresentation;
 import it.unive.lisa.util.representation.StructuredRepresentation;
 
@@ -13,14 +14,14 @@ public class ExtendedSignLattice implements BaseLattice<ExtendedSignLattice> {
 
     private int element;
 
-    private static ExtendedSignLattice TOP = new ExtendedSignLattice(0);
-    private static ExtendedSignLattice NON_ZERO = new ExtendedSignLattice(1);
-    private static ExtendedSignLattice NON_POS = new ExtendedSignLattice(2);
-    private static ExtendedSignLattice NON_NEG = new ExtendedSignLattice(3);
-    private static ExtendedSignLattice NEG = new ExtendedSignLattice(4);
-    private static ExtendedSignLattice ZERO = new ExtendedSignLattice(5);
-    private static ExtendedSignLattice POS = new ExtendedSignLattice(6);
-    private static ExtendedSignLattice BOTTOM = new ExtendedSignLattice(7);
+    public static ExtendedSignLattice TOP = new ExtendedSignLattice(0);
+    public static ExtendedSignLattice NON_ZERO = new ExtendedSignLattice(1);
+    public static ExtendedSignLattice NON_POS = new ExtendedSignLattice(2);
+    public static ExtendedSignLattice NON_NEG = new ExtendedSignLattice(3);
+    public static ExtendedSignLattice NEG = new ExtendedSignLattice(4);
+    public static ExtendedSignLattice ZERO = new ExtendedSignLattice(5);
+    public static ExtendedSignLattice POS = new ExtendedSignLattice(6);
+    public static ExtendedSignLattice BOTTOM = new ExtendedSignLattice(7);
 
 
     public ExtendedSignLattice(int element) {
@@ -31,9 +32,9 @@ public class ExtendedSignLattice implements BaseLattice<ExtendedSignLattice> {
     @Override
     public ExtendedSignLattice lubAux(ExtendedSignLattice other) throws SemanticException {
         if (this.equals(other)) return this;
-        if (this.equals(ZERO) && other.equals(POS) || this.equals(POS) && other.equals(ZERO)) return NON_NEG;
-        if (this.equals(ZERO) && other.equals(NEG) || this.equals(NEG) && other.equals(ZERO)) return NON_POS;
-        if (this.equals(POS) && other.equals(NEG) || this.equals(NEG) && other.equals(POS)) return NON_ZERO;
+        if ((this.equals(ZERO) && other.equals(POS)) || (this.equals(POS) && other.equals(ZERO))) return NON_NEG;
+        if ((this.equals(ZERO) && other.equals(NEG)) || (this.equals(NEG) && other.equals(ZERO))) return NON_POS;
+        if ((this.equals(POS) && other.equals(NEG)) || (this.equals(NEG) && other.equals(POS))) return NON_ZERO;
         if (this.lessOrEqualAux(other)) return other;
         if (other.lessOrEqualAux(this)) return this;
         return TOP;
@@ -44,9 +45,9 @@ public class ExtendedSignLattice implements BaseLattice<ExtendedSignLattice> {
         if (this.equals(other)) return this;
         if (this.lessOrEqualAux(other)) return this;
         if (other.lessOrEqualAux(this)) return other;
-        if (this.equals(NON_POS) && other.equals(NON_NEG) || this.equals(NON_NEG) && other.equals(NON_POS)) return ZERO;
-        if (this.equals(NON_POS) && other.equals(NON_ZERO) || this.equals(NON_ZERO) && other.equals(NON_POS)) return NEG;
-        if (this.equals(NON_NEG) && other.equals(NON_ZERO) || this.equals(NON_ZERO) && other.equals(NON_NEG)) return POS;
+        if ((this.equals(NON_POS) && other.equals(NON_NEG)) || (this.equals(NON_NEG) && other.equals(NON_POS))) return ZERO;
+        if ((this.equals(NON_POS) && other.equals(NON_ZERO)) || (this.equals(NON_ZERO) && other.equals(NON_POS))) return NEG;
+        if ((this.equals(NON_NEG) && other.equals(NON_ZERO)) || (this.equals(NON_ZERO) && other.equals(NON_NEG))) return POS;
         return BOTTOM;
     }
 
@@ -57,6 +58,35 @@ public class ExtendedSignLattice implements BaseLattice<ExtendedSignLattice> {
         if (this.equals(NEG)) return other.equals(NON_POS) || other.equals(NON_ZERO);
         if (this.equals(POS)) return other.equals(NON_NEG) || other.equals(NON_ZERO);
         return false;
+    }
+
+    public Satisfiability eq(ExtendedSignLattice other) {
+        if (this.isBottom() || other.isBottom())
+            return Satisfiability.BOTTOM;
+        if (this.isTop() || other.isTop())
+            return Satisfiability.UNKNOWN;
+        if (this.equals(ZERO) && other.equals(ZERO))
+            return Satisfiability.SATISFIED;
+        try {
+            if (this.glb(other).isBottom())
+                return Satisfiability.NOT_SATISFIED;
+        } catch (SemanticException e) {
+            return Satisfiability.UNKNOWN;
+        }
+        return Satisfiability.UNKNOWN;
+    }
+
+    public Satisfiability gt(ExtendedSignLattice other) {
+        if (this.isBottom() || other.isBottom()) return Satisfiability.BOTTOM;
+        if (this.isTop() || other.isTop()) return Satisfiability.UNKNOWN;
+
+        if (this.equals(POS) && (other.equals(ZERO) || other.equals(NEG) || other.equals(NON_POS))) return Satisfiability.SATISFIED;
+        if ((this.equals(ZERO) || this.equals(NON_NEG)) && other.equals(NEG)) return Satisfiability.SATISFIED;
+
+        if ((this.equals(NEG) || this.equals(ZERO)) && (other.equals(ZERO) || other.equals(POS) || other.equals(NON_NEG))) return Satisfiability.NOT_SATISFIED;
+        if (this.equals(NON_POS) && other.equals(POS)) return Satisfiability.NOT_SATISFIED;
+
+        return Satisfiability.UNKNOWN;
     }
 
     @Override
@@ -71,14 +101,14 @@ public class ExtendedSignLattice implements BaseLattice<ExtendedSignLattice> {
 
     @Override
     public StructuredRepresentation representation() {
-        if (this == BOTTOM) return Lattice.bottomRepresentation();
-        if (this == TOP) return Lattice.topRepresentation();
-        if (this == NEG) return new StringRepresentation("<=0");
-        if (this == ZERO) return new StringRepresentation("=0");
-        if (this == POS) return new StringRepresentation(">=0");
-        if (this == NON_NEG) return new StringRepresentation(">=0");
-        if (this == NON_POS) return new StringRepresentation("<=0");
-        if (this == NON_ZERO) return new StringRepresentation("!=0");
+        if (this.isBottom()) return Lattice.bottomRepresentation();
+        if (this.isTop()) return Lattice.topRepresentation();
+        if (this.equals(NEG)) return new StringRepresentation("<0");
+        if (this.equals(ZERO)) return new StringRepresentation("=0");
+        if (this.equals(POS)) return new StringRepresentation(">0");
+        if (this.equals(NON_NEG)) return new StringRepresentation(">=0");
+        if (this.equals(NON_POS)) return new StringRepresentation("<=0");
+        if (this.equals(NON_ZERO)) return new StringRepresentation("!=0");
         return new StringRepresentation("?");
     }
 
